f6e874cfa773e5d0dbac06ce29056a996cbc53c8
mars0i <mabrams@uab.edu>
2013-10-26 23:55:45 -0500

    added neural nets matrix Grossberg source

---
+++ src/neural-nets-test.clj
@@ -0,0 +1,16 @@
+
+;(use 'criterium.core)
+;(use 'clojure.core.matrix)
+
+; do this:
+;(load-file "../popco2/neural-nets.clj")
+; then do this:
+;(set-current-implementation ...)
+; :persistent-vector
+; :ndarray
+; :vectorz
+
+(print (format "%n==============================%nImplementation: %s:%n" (current-implementation)))
+(bench-settle 250  0.035)
+(bench-settle 500  0.035)
+(bench-settle 1000 0.035)
