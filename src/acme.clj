2b41a73ca776d9a84537a61538d85be91ecb5dcb
mars0i <mabrams@uab.edu>
2013-10-30 11:32:57 -0500

    added comment documenting how Propn should be used (since can't docstring defrecord)

--- src/acme.clj
+++ src/acme.clj
@@ -1,1 +1,6 @@
+
 (defrecord Propn [pred args id])
+;; pred: predicate - should be keyword with initial uppercase
+;; args: array of argments, which could either be objects (keyword starting with "ob-") or names of other propositions
+;; id: name for proposition, which should start with all upper domain id, then dash (e.g. :CV-blah-blah)
+;; (question: should I consider putting propns raw as arguments??)
