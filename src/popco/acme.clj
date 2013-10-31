3eeed3a899843801eada7ec3ec6c7ce6b7ca6fa5
mars0i <mabrams@uab.edu>
2013-10-31 14:25:11 -0500

    added docstring for ->Propn.

--- src/popco/acme.clj
+++ src/popco/acme.clj
@@ -1,7 +1,11 @@
 (ns popco)
 
 (defrecord Propn [pred args id])
-;; pred: predicate - should be keyword with initial uppercase
-;; args: array of argments, which could either be objects (keyword starting with "ob-") or names of other propositions
-;; id: name for proposition, which should start with all upper domain id, then dash (e.g. :CV-blah-blah)
-;; (question: should I consider putting propns raw as arguments??)
+
+;; Add doc-string for class to the Clojure constructor function:
+(alter-meta! #'->Propn update-in [:doc] str 
+  "\n  pred: Predicate - should be keyword with initial uppercase.
+  args: Array of argments, which could either be objects (keyword starting with 'ob-')
+        or names of other propositions.
+  id:   Name for proposition, which should start with all upper domain id, then dash
+        (e.g. :CV-blah-blah).")
