fc573e496b8ea37f9d24917f1d16b04268649b14
mars0i <mabrams@uab.edu>
2013-10-30 23:30:33 -0500

    Rearranged files, project.clj to be like normal lein app.

--- project.clj
+++ project.clj
@@ -1,10 +1,12 @@
-(defproject mars0i/popco "0.0.0-SNAPSHOT"
+(defproject popco-x "0.0.0-SNAPSHOT"
   :url "https://github.com/mars0i/popco-x"
   :license {:name "to be filled in"
             :url "to be filled in"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
-  :profiles {:dev {:dependencies [;[org.clojure/tools.macro "0.1.5"]
+  :main popco
+  :profiles {:dev {:dependencies [;[criterium/criterium "0.4.2"]
+                                  [org.clojure/tools.macro "0.1.5"]
                                   [net.mikera/core.matrix "0.13.1"]
-                                  [net.mikera/vectorz-clj "0.14.0"]
-                                  ;[criterium/criterium "0.4.2"]
-                                 ]}})
+                                  [net.mikera/vectorz-clj "0.14.0"]]
+                   :source-paths ["src/popco"]
+                   }})
