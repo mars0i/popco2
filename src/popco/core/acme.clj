7245b64e8f2db210b8bed13d5cfdda589045d4ff
mabrams <mabrams@uab.edu>
2013-11-02 14:38:29 -0500

    First version of multimethod-based proposition-matching. Doesn't work yet with crime3propns.clj.  How should propositions be represented as arguments?  keyword lookup?  Real propns? Then need to define symbols first.  (And in that case do I need id tags?)

--- src/popco/core/acme.clj
+++ src/popco/core/acme.clj
@@ -13,19 +13,24 @@
 (ug/add-to-docstr ->Obj
   "\n  id: Name for proposition, which should start with 'ob-' [OBSOLETE?].")
 
+(declare propns-match? args-match?)
+
 (defn propns-match?
   [p1 p2]
   (let [args1 (:args p1)
         args2 (:args p2)]
   (and (= (count args1) (count args2))
-       (or (
-            
-            )))))
+       (every? identity (map args-match? args1 args2)))))
 
+(defmulti args-match? (fn [x y] [(class x) (class y)]) )
+(defmethod args-match? [Obj Propn] [_ _] false)
+(defmethod args-match? [Propn Obj] [_ _] false)
+(defmethod args-match? [Obj Obj] [_ _] true)
+(defmethod args-match? [Propn Propn] [p1 p2]
+  (every? identity (map propns-match? (:args p1) (:args p2))))
 
 (defn matched-propn-pairs
   [pset1 pset2]
-
   (for [p1 pset1
         p2 pset2
         :when (propns-match? p1 p2)]
--- src/utils/general.clj
+++ src/utils/general.clj
@@ -1,6 +1,9 @@
 (ns utils.general ; Utility functions handy for any Clojure program
   (:require [clojure.pprint :only [*print-right-margin*]]))
 
+;(defn unlock-and-load [path-or-ns]
+;  (load-file (pathify path-or-ns))
+;  (use (nsify path-or-ns)))
 
 (defn set-pprint-width 
   "Sets width for pretty-printing with pprint and pp."
