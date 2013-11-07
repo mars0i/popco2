0c2cb658cc4301d63646064c74517512161a508c
mabrams <mabrams@uab.edu>
2013-11-07 12:00:38 -0600

    renamed matching fns. replaced sets with vecs since analogues must be distinguished, and preds and objs can be shared beween analogues.

--- src/popco/core/acme.clj
+++ src/popco/core/acme.clj
@@ -32,16 +32,19 @@
 
 (declare propns-match? args-match?)
 
-(defn matched-propn-pairs
-  "Returns a vector of 2-element sets, each containing two propositions
+;; Note that order within pairs matters.  It preserves the distinction
+;; between the two analogue structures.
+(defn match-propns
+  "Returns a sequence of 2-element sequences, each containing two propositions
   that match according to propns-match?.  These are propositions that are
   isomorphic in the ACME sense and that can be used to construct map nodes."
   [pset1 pset2]
-  (vec (for [p1 pset1
+  (for [p1 pset1
         p2 pset2
         :when (propns-match? p1 p2)]
-    [p1 p2])))
+    [p1 p2]))
 
+;; similar to deep-isomorphic-arglists in popco 1
 (defn propns-match?
   [p1 p2]
   (let [args1 (:args p1)
@@ -49,7 +52,7 @@
   (and (= (count args1) (count args2))
        (every? identity (map args-match? args1 args2)))))
 
-;; write as single conditional?
+;; similar to isomorphic-args in popco 1
 (defmulti  args-match? (fn [x y] [(class x) (class y)]) )
 (defmethod args-match? [Obj Propn] [_ _] false)
 (defmethod args-match? [Propn Obj] [_ _] false)
@@ -57,23 +60,26 @@
 (defmethod args-match? [Propn Propn] [p1 p2] (propns-match? p1 p2))
 
 
-(declare match-args match-propn-pair-components)
+(declare match-args match-propn-components)
 
-(defn match-propn-pairs
-  "Return vector of matched Propns, predicates, and Objs from sequence 
+;; Note that order within pairs matters.  It preserves the distinction
+;; between the two analogue structures, and allows predicates and objects
+;; to have the same names in different analogue structures (sets don't allow).
+(defn match-propn-components-too
+  "Return sequence of matched Propns, predicates, and Objs from sequence 
   of pairs of Propns."
   [pairs]
-  (mapcat match-propn-pair-components pairs))
+  (mapcat match-propn-components pairs))
 
-(defn match-propn-pair-components
+(defn match-propn-components
   [[p1 p2]]
-  [#{p1 p2}
-   #{(:pred p1) (:pred p2)}
+  [[p1 p2]
+   [(:pred p1) (:pred p2)]
    (mapcat match-args (:args p1) (:args p2))])
 
 (defmulti  match-args (fn [x y] [(class x) (class y)]))
-(defmethod match-args [Obj Obj] [o1 o2] #{o1 o2})
-(defmethod match-args [Propn Propn] [p1 p2] (match-propn-pair-components p1 p2))
+(defmethod match-args [Obj Obj] [o1 o2] [o1 o2])
+(defmethod match-args [Propn Propn] [p1 p2] (match-propn-components p1 p2))
 ; not right--doesn't handle dupe propn matchs right.  That needs to be
 ; in weights.
 
@@ -94,7 +100,7 @@
 
 
 
-;; Handy for displaying output of matched-propn-pairs:
+;; Handy for displaying output of match-propns:
 (defn pair-ids
   "Return sequence of pairs of :id fields of objects from sequence prs of pairs."
   [prs]
