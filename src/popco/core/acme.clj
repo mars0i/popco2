03c8b935e82f3982ffcb285cfd6a9d4e9dbd0ad9
mabrams <mabrams@uab.edu>
2013-11-08 12:56:40 -0600

    added distinct to remove dupe pairings in match-propn-components-too, and changes in comments, docstrings, etc.

--- src/popco/core/acme.clj
+++ src/popco/core/acme.clj
@@ -1,20 +1,33 @@
-;; ACME: an implementation of Holyoak & Thagard's (1989) ACME method
-;; of constructing a constraint satisfaction network from two sets
-;; of simple representations of entertained propositions.
-
 (ns popco.core.acme
   (:use popco.core.lot)
   (:import [popco.core.lot Propn Pred Obj])
   (:require [utils.general :as ug]))
+
+;; ACME: an implementation of Holyoak & Thagard's (1989) ACME method
+;; of constructing a constraint satisfaction network from two sets
+;; of simple representations of entertained propositions.
+
+
+;;; TODO match-components-too is broken--it runs, but isn't behaving correctly.
+
+;;; TODO construct link matrix and weight matrix with positive weights
+;;; TODO add negative weights to weight matrix
+;;; NOTE for the analogy net we probably don't really need the link
+;;; matrix, strictly speaking, because there are no zero-weight links.
+;;; For the belief network, however, we need to allow zero-weight links,
+;;; so a link matrix is needed.
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; First step: Find out which propositions can be paired up because they're isomorphic:
 
 (declare propns-match? args-match?)
 
 ;; Note that order within pairs matters.  It preserves the distinction
 ;; between the two analogue structures.
 (defn match-propns
-  "Returns a sequence of 2-element sequences, each containing two propositions
+  "Returns a (lazy) sequence of 2-element sequences, each containing two propositions
   that match according to propns-match?.  These are propositions that are
-  isomorphic in the ACME sense and that can be used to construct map nodes."
+  isomorphic in the ACME sense, and that can be used to construct map nodes."
   [pset1 pset2]
   (for [p1 pset1
         p2 pset2
@@ -23,6 +36,9 @@
 
 ;; similar to deep-isomorphic-arglists in popco 1
 (defn propns-match?
+  "Tests whether two propositions are isomorphic in the ACME sense.
+  Recursively checks propositions given as arguments of propositions.
+  Returns true if they're isomorphic, false otherwise."
   [p1 p2]
   (let [args1 (:args p1)
         args2 (:args p2)]
@@ -37,35 +53,40 @@
 (defmethod args-match? [Propn Propn] [p1 p2] (propns-match? p1 p2))
 
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Second step: For all isomporphic propositions, pair up their components
+;; By separating step 1 and step 2, we do some redundant work, but it makes
+;; the logic a bit simpler, for now, and we'll only be doing this once for the
+;; whole population at the beginning of each simulation.
+
+;; i.e. we feed the return value of match-propns, above, to 
+;; match-propn-components-too, below.
+
 (declare match-args match-propn-components)
 
+;; TODO: BROKEN
 ;; Note that order within pairs matters.  It preserves the distinction
 ;; between the two analogue structures, and allows predicates and objects
 ;; to have the same names in different analogue structures (sets don't allow).
 (defn match-propn-components-too
-  "Return sequence of matched Propns, predicates, and Objs from sequence 
-  of pairs of Propns."
+  "Return a (lazy) sequence of unique pairs of matched Propns, predicates, and Objs 
+  from a sequence of of pairs of Propns.  The resulting pairs represent the 'sides' 
+  of map nodes."
   [pairs]
-  (mapcat match-propn-components pairs))
+  (distinct
+    (mapcat match-propn-components pairs)))
 
 (defn match-propn-components
   [[p1 p2]]
   [[p1 p2]
-   [(:pred p1) (:pred p2)]
-   (mapcat match-args (:args p1) (:args p2))])
+   [(:pred p1) (:pred p2)] ; predicates always match if the proposition matched
+   (mapcat match-args (:args p1) (:args p2))]) ; args match if they're objects, propns require further checking
 
 (defmulti  match-args (fn [x y] [(class x) (class y)]))
 (defmethod match-args [Obj Obj] [o1 o2] [o1 o2])
 (defmethod match-args [Propn Propn] [p1 p2] (match-propn-components p1 p2))
-; not right--doesn't handle dupe propn matchs right.  That needs to be
-; in weights.
 
-
-(defn node-to-index-map
-  "Given a sequence of node info entries (e.g. Propns, pairs of Propns or 
-  Objs, etc.), returns a hashmap from node info entries to indexes."
-  [nodes-info]
-  (zipmap nodes-info (range (count nodes-info))))
+;; NEXT TWO WILL WORK FOR THE PROPN NET, TOO, SO THEY MIGHT BE MOVED LATER.
 
 (defn index-to-node-vec
   "Given a sequence of node info entries (e.g. Propns, pairs of Propns or 
@@ -73,9 +94,17 @@
   [nodes-info]
   (vec nodes-info))
 
+;; match-propn-components gives us a sequence of node info pairs which can be turned into
+;; a vector of some kind, and then we can index into it.  It's also useful
+;; to be able to look up the pairs from their indexes.  This function generates
+;; a hashmap that gives us that.  We can also use it with the belief network.
 
-
-
+(defn node-to-index-map
+  "Given a sequence of node info entries (e.g. Propns, pairs of Propns or 
+  Objs, etc.), returns a hashmap from node info entries to indexes.  Allows
+  reverse lookup of the node entries' indexes."
+  [nodes-info]
+  (zipmap nodes-info (range (count nodes-info))))
 
 ;; Handy for displaying output of match-propns:
 (defn pair-ids
