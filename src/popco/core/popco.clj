814ea77381790cb376714ba6aa753a850a4f337d
mabrams <mabrams@uab.edu>
2013-11-11 11:48:28 -0600

    Add automatic set of *pprint-width* to terminal width.

--- src/popco/core/popco.clj
+++ src/popco/core/popco.clj
@@ -8,6 +8,9 @@
   (:gen-class))
 ;; cf. https://kotka.de/blog/2010/02/gen-class_how_it_works_and_how_to_use_it.html
 
+;; set pretty-print width to terminal width
+(set-pprint-width (Integer/valueOf (System/getenv "COLUMNS"))) ; or read-string
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
