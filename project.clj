4de550b52457d11eeba539447787ce4654d59f18
mabrams <mabrams@uab.edu>
2013-11-19 11:16:23 -0600

    Added clatrix dep, for experimentation.

--- project.clj
+++ project.clj
@@ -4,12 +4,12 @@
             :url "to be filled in"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :main popco.core.popco ; this is what lein run will look for -main in
-  :profiles {:dev {:dependencies [;[criterium/criterium "0.4.2"] ; benchmarking
+  :profiles {:dev {:dependencies [;[criterium/criterium "0.4.2"]
+                                  [slingshot "0.10.3"][org.jblas/jblas "1.2.3"][clatrix "0.3.0"]
                                   [org.clojure/math.combinatorics "0.0.7"]
                                   [org.clojure/tools.macro "0.1.5"]
                                   [net.mikera/core.matrix "0.15.0"]
                                   [net.mikera/vectorz-clj "0.17.0"]]
-                   ;:source-paths ["src/popco/core" "src/popco/sims" "src/utils"] ; where load will look for source files
                    :source-paths ["src"] ; where load will look for source files
                    }}
    :jvm-opts ["-Dclojure.compiler.disable-locals-clearing=true"]
