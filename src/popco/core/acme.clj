8da26cade83af5af599e785e508bedec4c4aa264
mars0i <mabrams@uab.edu>
2013-11-25 23:48:04 -0600

    added comment

--- src/popco/core/acme.clj
+++ src/popco/core/acme.clj
@@ -478,6 +478,9 @@
   [nn-stru]
   (print (format-nn-stru nn-stru)))
 
+;; Trying to do this with cl-format was too difficult.
+;; The crude strategy of simply doing a regex replace on a formatted
+;; string works well enough, assuming that numbers satsify certain constraints.
 (defn dotprint-nn-stru
   "Pretty-print the matrix in nn-stru with associated row, col info,
   replacing zeros with dots, so that it's easy to distinguish zeros
