ad28c6e169325698e1678350d041a27b6a285641
mars0i <mabrams@uab.edu>
2013-12-12 23:52:55 -0600

    experimental project.clj uses my variant libs

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject popco-x "0.0.0-SNAPSHOT"
+(defproject popco-x "0.0.1-SNAPSHOT"
   :url "https://github.com/mars0i/popco-x"
   :license {:name "to be filled in"
             :url "to be filled in"}
@@ -8,8 +8,11 @@
                                   [slingshot "0.10.3"][org.jblas/jblas "1.2.3"][clatrix "0.3.0"]
                                   ;[org.clojure/math.combinatorics "0.0.7"]
                                   [org.clojure/tools.macro "0.1.5"]
-                                  [net.mikera/core.matrix "0.15.0"]
-                                  [net.mikera/vectorz-clj "0.17.0"]]
+                                  ;[net.mikera/core.matrix "0.15.0"]
+                                  [mars0i/core.matrix "0.16.1-SNAPSHOT"]
+                                  ;[net.mikera/vectorz-clj "0.17.0"]
+                                  [mars0i/vectorz-clj "0.17.0-SNAPSHOT"]
+                                 ]
                    :source-paths ["src"] ; where load will look for source files
                    }}
    :jvm-opts ["-Dclojure.compiler.disable-locals-clearing=true"]
