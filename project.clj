f0b787ca9ee4b0064f8f10e6f57ceadb781c40a0
mars0i <mabrams@uab.edu>
2013-12-20 13:01:11 -0600

    Added load for latest clatrix that I deployed

--- project.clj
+++ project.clj
@@ -5,10 +5,8 @@
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :main popco.core.popco ; this is what lein run will look for -main in
   :profiles {:dev {:dependencies [;[criterium/criterium "0.4.2"]
-                                  ;; NEXT LINE has to be fixed because clatrix 0.3.0 in clojars has 
-                                  ;; a dep on core.matrix 0.7.2, which lacks important things, e.g. 
-                                  ;; causes mmul to be not found.  ('lein deps :tree' is useful)
-                                  ;[slingshot "0.10.3"][org.jblas/jblas "1.2.3"][clatrix "0.3.0"]
+                                  ;[slingshot "0.10.3"][org.jblas/jblas "1.2.3"][mars0i/clatrix "0.4.0-SNAPSHOT"]
+                                  [mars0i/clatrix "0.4.0-SNAPSHOT"]
                                   [org.clojure/tools.macro "0.1.5"]
                                   ;[net.mikera/core.matrix "0.15.0"]
                                   ;[net.mikera/vectorz-clj "0.17.0"]
