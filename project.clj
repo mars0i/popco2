1b1de5fb1c0c61b3061efb178b762723ed028a35
mars0i <mabrams@uab.edu>
2013-12-20 22:45:42 -0600

    Added dev version of clatrix to project.clj

--- project.clj
+++ project.clj
@@ -5,11 +5,11 @@
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :main popco.core.popco ; this is what lein run will look for -main in
   :profiles {:dev {:dependencies [;[criterium/criterium "0.4.2"]
-                                  ;[slingshot "0.10.3"][org.jblas/jblas "1.2.3"][mars0i/clatrix "0.4.0-SNAPSHOT"]
-                                  [mars0i/clatrix "0.4.0-SNAPSHOT"]
                                   [org.clojure/tools.macro "0.1.5"]
+                                  ;[slingshot "0.10.3"][org.jblas/jblas "1.2.3"][mars0i/clatrix "0.4.0-SNAPSHOT"]
                                   ;[net.mikera/core.matrix "0.15.0"]
                                   ;[net.mikera/vectorz-clj "0.17.0"]
+                                  [mars0i/clatrix "0.4.0-SNAPSHOT"]
                                   [mars0i/core.matrix "0.16.1-SNAPSHOT"]
                                   [mars0i/vectorz-clj "0.17.1-SNAPSHOT"]
                                  ]
