66d98ff82bf10f3eebbfca8070e313d5e6140cb9
mars0i <mabrams@uab.edu>
2013-12-30 23:43:06 -0600

    Defined make-propn-net.

--- src/popco/nn/propn.clj
+++ src/popco/nn/propn.clj
@@ -5,3 +5,12 @@
             [clojure.core.matrix :as mx])
   (:import [popco.core.lot Propn]
            [popco.nn.core PropnNet]))
+
+
+(defn make-propn-net
+  [propnseq]
+  (let [dim (count propnseq)]
+    (nn/map->PropnNet
+      (assoc 
+        (nn/make-nn-core propnseq)
+        :wt-mat (mx/new-matrix dim dim)))))
