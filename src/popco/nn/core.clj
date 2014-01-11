89c329c1264d43a20ec067de65bba68479d1f488
mabrams <mabrams@uab.edu>
2014-01-11 14:39:26 -0600

    Added def of Person

--- src/popco/nn/core.clj
+++ src/popco/nn/core.clj
@@ -120,3 +120,12 @@
   [node-seq]
   { :node-vec (vec node-seq)
     :id-to-idx (make-id-to-idx-map (map :id node-seq)) }) ; index order will be same as node-seq's order
+
+(defrecord Person [propn-mask propn-activns propn-net analogy-mask analogy-activns])
+(ug/add-to-docstr ->Person
+   "Makes a POPCO Person, with these fields:
+   propn-mask -      vector of 1's (propn is entertained) and 0's (isn't)
+   propn-activns -   vector of activation values for nodes in propn net
+   propn-net -       constraint satisfaction net weights between propns
+   analogy-mask -    vector of 1's (mapnode is present) or 0's (it's absent)
+   analogy-activns - activation values of nodes in analogy net" )
