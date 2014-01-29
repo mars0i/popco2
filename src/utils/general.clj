d69119b89362ddac17d7b1bcfa79a2e06a9bd878
mars0i <mabrams@uab.edu>
2014-01-28 19:56:12 -0600

    Added multiple-list version of domap (was mapc)

--- src/utils/general.clj
+++ src/utils/general.clj
@@ -2,10 +2,22 @@
   (:require [clojure.pprint :only [*print-right-margin*]]))
 
 ;; Simple Clojure version of mapc.
-;; Needs to be exanded to deal with multiple seqs
-(defn domap [f s]
-  (doseq [e s]
+(defn domap1 [f coll]
+  [f coll]
+  (doseq [e coll] 
     (f e)))
+
+;; Clojure mapc with multiple list args
+;; Is this any more efficient than Clojure map ?
+;; After all, it constructs a new seq combining the arg seqs
+;; How is this better than constructing a seq of results?
+;; It does enforce that only side effects will matter--only nil is returned.
+(defmacro domap
+  [f & colls]
+  (let [params (vec (repeatedly (count colls) gensym))] ; one param name for each seq arg
+    `(let [argvecs# (map vector ~@colls)]               ; seq of vecs of interleaved elements
+       (doseq [~params argvecs#]
+         (~f ~@params)))))
 
 (defn unlocknload 
   "Given a symbol representing a namespace, converts the symbol
