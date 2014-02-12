90199204c90641f15e33cc43f421419a3fb4d2f2
mabrams <mabrams@uab.edu>
2014-02-12 12:24:49 -0600

    Revised receive-propn to iterate through extended families
    i.e. if a propn is newly received, then there may be other propns
    that can now participate in analogies because it's there.  So we
    need to try to form analogies using all of the propns in extended
    families of which the sent propn is a member.

--- src/popco/core/communic.clj
+++ src/popco/core/communic.clj
@@ -7,21 +7,24 @@
 (declare receive-propn add-to-propn-net try-add-to-analogy-net propn-already-unmasked? propn-still-masked?  
          propn-components-already-unmasked?  ids-to-poss-mn-id unmask-mapnode-extended-family!)
 
+;; TODO UNVERIFIED
 ;; TODO this or some other function will eventually have to add in other effects
 ;; on the proposition network in order to add/subtract activation via weight to
 ;; the SALIENT node (which needs to be added to the net!).
-;; TODO also find propns that the new propn participates in, and try to add them to analogy net as well
 (defn receive-propn
-  [pers propn]
-  (when (propn-still-masked? pers propn)
-    (add-to-propn-net pers propn)
-    (try-add-to-analogy-net pers propn)))
-;; Note: We don't really need to test whether propn is still masked for the sake
-;; of add-to-propn-map, since it's cheap and it doesn't matter if it's performed
-;; redundantly.  try-add-to-analogy-net is more expensive--worth testing
-;; before going through any work.
+  "ADD DOCSTRING"
+  [pers recd-propn]
+  (when (propn-still-masked? pers recd-propn) ; if recd propn already unmasked, the rest is already done
+    (add-to-propn-net pers recd-propn)
+    (let [propn-to-extended-fams-ids (:propn-to-extended-fams-ids (:propn-net pers))
+          fams (propn-to-extended-fams-ids recd-propn)]
+      (doseq [fam fams                           ; loop through all extended fams containing this propn
+              propn fam]                         ; and each propn in that family
+        (try-add-to-analogy-net pers propn)))))  ; see whether we can now add analogies using it
+;; TODO ? this last loop redundantly tries to add analogies for recd-propn repeatedly, though will not do much after the first time
 
 (defn add-to-propn-net
+  "ADD DOCSTRING"
   [pers propn]
   (let [pnet (:propn-net pers)]
     (nn/unmask! (:propn-mask pers) ((:id-to-idx pnet) propn))))
@@ -37,14 +40,6 @@
                         (ids-to-poss-mn-id pers propn a-propn))]
           (unmask-mapnode-extended-family! pers mn-id))))))            ; and all extended family mapnodes
 
-(defn propn-already-unmasked?
-  "Return true if, in person (first arg), propn (second arg) exists in the
-  proposition net in the sense that it has been unmasked; false otherwise."
-  [{{id-to-idx :id-to-idx} :propn-net ; bind field of propn-net of person that's passed as 2nd arg
-    propn-mask :propn-mask}           ; bind propn-mask of person
-   propn]
-  (nn/node-unmasked? propn-mask (id-to-idx propn)))
-
 (defn propn-still-masked?
   "Return true if, in person (first arg), propn (second arg) doesn't exist
    in the proposition net in the sense that it's masked; false otherwise."
@@ -52,6 +47,14 @@
     propn-mask :propn-mask}           ; bind propn-mask of person
    propn]
   (nn/node-masked? propn-mask (id-to-idx propn)))
+
+(defn propn-already-unmasked?
+  "Return true if, in person (first arg), propn (second arg) exists in the
+  proposition net in the sense that it has been unmasked; false otherwise."
+  [{{id-to-idx :id-to-idx} :propn-net ; bind field of propn-net of person that's passed as 2nd arg
+    propn-mask :propn-mask}           ; bind propn-mask of person
+   propn]
+  (nn/node-unmasked? propn-mask (id-to-idx propn)))
 
 (defn propn-components-already-unmasked?
   "Return true if, in person (first arg), propn (second arg) is a possible
@@ -71,6 +74,7 @@
   (an/ids-to-poss-mapnode-id propn1-id propn2-id id-to-idx))
 
 (defn unmask-mapnode-extended-family!
+  "ADD DOCSTRING"
   [{{propn-mn-to-ext-fam-idxs :propn-mn-to-ext-fam-idxs} :analogy-net ; bind index map from analogy-net in person
     analogy-mask :analogy-mask} ; bind mask in person
    mn-id]
