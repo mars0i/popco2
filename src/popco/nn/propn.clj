839df0d942f4410fb90607b73a671bb1cbf442b7
mabrams <mabrams@uab.edu>
2014-02-12 12:21:17 -0600

    :propn-extended-fams-idxs now propn-extended-fams-ids
    i.e. the seqs of things associated with a propn id are now ids,
    because that is that (currently) the functions called by receive-propn
    need.  Reluctant to do it all in terms of indexes, which are hard to
    investigate.

--- src/popco/nn/propn.clj
+++ src/popco/nn/propn.clj
@@ -1,3 +1,8 @@
+;; propn.clj
+;; Functions for creating and working with PropnNets.
+;; Further documentation on PropnNets can be read and maintained in net.clj
+;; See lot.clj for documentation on Propns and their components.
+
 (ns popco.nn.propn
   (:use popco.core.lot)
   (:require [popco.nn.nets :as nn]
@@ -6,8 +11,7 @@
   (:import [popco.core.lot Propn]
            [popco.nn.nets PropnNet]))
 
-
-(declare make-propn-to-extended-descendant-propn-idxs propn-extended-descendant-propns make-propn-to-extended-fams-idxs make-propn-net)
+(declare make-propn-to-extended-descendant-propn-idxs propn-extended-descendant-propns make-propn-to-extended-fams-ids make-propn-net)
 
 ;; TODO Add the SALIENT node
 (defn make-propn-net
@@ -21,23 +25,25 @@
                                                     propnseq (:id-to-idx nncore)))]
     (nn/map->PropnNet
       (assoc pnetmap
-             :propn-to-extended-fams-idxs (make-propn-to-extended-fams-idxs pnetmap)))))
+             :propn-to-extended-fams-ids (make-propn-to-extended-fams-ids pnetmap)))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; TODO NOT VERIFIED
-(defn make-propn-to-extended-fams-idxs
-  "Create a map from each propn id to sets of indexes of all propns that bear 
-  extended family relations to it, whether upward or downward."
+(defn make-propn-to-extended-fams-ids
+  "Create a map from each propn id to a vector of seqs of ids (not indexes) of propns.
+  Each such seq records the ids of propns in one extended family of which the key propn
+  is a member, whether it is the matriarch of the family or not."
   [pnet]
   (let [idx-to-id (:id-vec pnet)
         idx-fams (vals (:propn-to-descendant-propn-idxs pnet))] ; we already have extended fams as idxs, so use them
     (group-by 
-      (comp idx-to-id first)            ; group/hashmap on id of the first idx
-      (mapcat ug/rotations idx-fams)))) ; list all rotations of each idx fam in order to put each id first
+      first            ; group/hashmap on id of the first idx
+      (mapcat ug/rotations 
+              (map #(map idx-to-id %) idx-fams))))) ; list all rotations of each idx fam in order to put each id first
 
 (defn make-propn-to-extended-descendant-propn-idxs
-  "Create a map from propn ids to seqs of indexes into the propn vector.
+  "Create a map from propn ids to seqs of indexes into the propn vector (not ids).
   For propn P, the seq contains its index, the indexes of any propns that
   are its args, propns that are their args, etc.  First arg is a collection
   of propns. Second arg is a map (or function) from propn ids to indexes
