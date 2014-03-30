40acf996568a0c451d43cdc73c0d036992427ef3
mars0i <mabrams@uab.edu>
2014-03-30 11:16:28 -0500

    test files generated in POPCO 1 by crime3analogynetPopco2test.lisp

---
+++ src/popco/test/FromPopco1CRIME-NOLIVING.clj
@@ -0,0 +1,12 @@
+(def from-popco1 ' 
+([:CB-AP :CB-VPP 0.0 0.0]
+ [:CB-CPC :CB-DTP 0.0 0.0]
+ [:CB-CPC :CB-VPP -0.0 -0.0]
+ [:CB-HLP :CB-VPP 0.0 0.0]
+ [:CB-HRP :CB-VPP 0.0 0.0]
+ [:CV-CA :CV-HA 0.0 0.0]
+ [:CV-CA :CV-RPA 0.0 0.0]
+ [:CV-IP :CV-RPA -0.0 -0.0]
+ [:CV-IP->-CV-RPA :CV-NA 0.0 0.0]
+ [:CV-NA :CV-SA->-CV-RPA 0.0 0.0]
+ [:CV-RPA :CV-SA -0.0 -0.0]))
