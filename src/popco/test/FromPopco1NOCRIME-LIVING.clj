e3725120b16967de8c78f603276b2e9263a414eb
mars0i <mabrams@uab.edu>
2014-03-31 14:27:16 -0500

    New popco 1 link lists have ns declarations

--- src/popco/test/FromPopco1NOCRIME-LIVING.clj
+++ src/popco/test/FromPopco1NOCRIME-LIVING.clj
@@ -1,12 +1,4 @@
+(ns popco.test.crime3)
+
 (def from-popco1 ' 
-([:B-AB :B-ABP 0.0 0.0]
- [:B-ABP :B-CPB -0.0 -0.0]
- [:B-ABP :B-HLB 0.0 0.0]
- [:B-ABP :B-HRP 0.0 0.0]
- [:B-CPB :B-DTP 0.0 0.0]
- [:V-HA :V-IA 0.0 0.0]
- [:V-IA :V-IPA 0.0 0.0]
- [:V-ICA :V-IPA -0.0 -0.0]
- [:V-ICA->-V-IPA :V-NA 0.0 0.0]
- [:V-IPA :V-QP -0.0 -0.0]
- [:V-NA :V-QP->-V-IPA 0.0 0.0]))
+())
