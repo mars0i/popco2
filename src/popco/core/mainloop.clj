58f7f42473a46a940478d7c6fd90bb8e5729930d
mars0i <mabrams@uab.edu>
2014-04-08 23:25:03 -0500

    Notes, etc.

--- src/popco/core/mainloop.clj
+++ src/popco/core/mainloop.clj
@@ -5,9 +5,32 @@
   (report-progress-to-console)
   [(inc tick)
    (->> population
+     (settle-analogy-nets)
      (update-propn-nets-from-analogy-nets)
-     (settle-nets)
+     (settle-propn-nets)
      (choose-conversers)
      (choose-utterances)
-     (transmit-utterances)
-     (update-analogy-nets))])
+     (transmit-utterances))])
+
+;; Notes:
+
+;; Maybe settle-analogy-net should be done just once at the beginning.
+;; This seems reasonable, and would be faster, though will produce results
+;; that differ from popco1 simply because communication can depend on
+;; subtle variations.  However, in existing popco1 analogy nets,
+;; there was (small) cycling behavior, which can affect propn links, and
+;; therefore everything.  This won't play a role if I pull this out
+;; of the main loop.  But also, then what should I do about cycles?
+;; set to their average values?
+
+;; Hmm well if I do take analogy-net settling out of the main loop,
+;; then the contribution of the analogy net to the propn net links
+;; would also be static, right?  Because the analogy net activations
+;; don't change.  So the only thing that would change in the propn net
+;; would be due to the bump that you get from receiving a propn, etc.
+
+
+;; Note popco1's update-analogy-net isn't needed.  It revised the
+;; structure of the analogy net in response to new propns.  That's
+;; now done once and for all at the beginning.  And transmit-utterance
+;; can cause the unmasking.  This is done in communic/receive-propn!.
