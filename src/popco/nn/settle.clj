ff0f335b5293def9ed6d3556c001b6d61683e79e
mabrams <mabrams@uab.edu>
2014-04-19 14:13:39 -0500

    Trivial reorganization within file.

--- src/popco/nn/settle.clj
+++ src/popco/nn/settle.clj
@@ -33,54 +33,61 @@
 ;; Convention: Vector names are all-lower-case.  Matrices have initial cap
 ;; in each component of the name.
 
-(declare clip-to-extrema dist-from-max dist-from-min)
+(declare next-activns settle-net settle-analogy-net settle-propn-net
+         update-propn-wts-from-analogy-activns update-person-nets update-nets
+         clip-to-extrema dist-from-max dist-from-min)
 
 (def settling-iters 5)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Network settling (with Grossberg algorithm)
+;; See settle.md for notes.
 
-;; NOTE: 
-;;
-;; The Grossberg algorithm does no normalization in anything like
-;; the probabilistic sense: That is, outputs are outputs; they're not scaled
-;; relative to other signals coming into the same node:  The only "averaging"
-;; comes from the weighting across links due to the network, and the scaling
-;; by distance from max and min.  Moreover, the link weights are absolute
-;; numbers; they are not themselves scaled relative to other link weights.
-;; (In essence, it's the job of the neural net settling process to do 
-;; something analogous to probabilistic normalization.)
-;;
-;; (Qualification: For the proposition network there is a kludgey method of
-;; scaling negative links relative to positive links since there are more
-;; negative links: We just give the negative links a lower abs weight.)
-;;
-;; WHAT THIS MEANS is that you can effectively "remove" links from the 
-;; network simply by masking the input vector, forcing some values to zero.
-;; If a node sends no activation over the wires, then it's just as if links
-;; from that node had weight zero--i.e. as if these links didn't exist.
-;; Nothing *else* about the network needs to change.
-;;
-;; AND this means that since the analogy network has certain links to a 
-;; map node if and only if the node exists, and never adds or removes links
-;; to a map node once it exists, we can create the analogy network once,
-;; including all map nodes possible given the set of propositions in the
-;; two analog structures.  Then we can "remove" map nodes and their
-;; links from the network simply by zeroing the map node activations in
-;; the input vector.  Or rather, we can add map nodes *and their links*
-;; to the analogy network simply by beginning to put nonzero activations 
-;; in the corresponding map nodes.  In this way, different persons can
-;; have effectively different analogy networks, corresponding to their
-;; different repertoires of propositions, while using a single set of
-;; analogy network weight matrices (which needn't slow down run time
-;; due to being modified later).
-;;
-;; Note: The proposition networks, by contrast, can't be treated this way,
-;; because they acquire new links between old nodes.
+(defn update-nets
+  "Implements a single timestep's (tick's) worth of network settling and updating of the
+  proposition network from the analogy network.  Returns the population in its new state
+  after these processes have been performed."
+  [persons]
+  (map update-person-nets persons))
+
+(defn update-person-nets
+  "Perform one tick's updating of the networks of a single person."
+  [pers]
+  (-> pers
+    (settle-analogy-net  settling-iters) ; is this step necessary??
+    (update-propn-wts-from-analogy-activns)
+    (settle-propn-net    settling-iters)))
+
+(defn update-propn-wts-from-analogy-activns
+  "Currently a noop; returns the person unchanged."
+  [pers]
+  pers)
+
+(defn settle-analogy-net
+  "Return person pers with its analogy net updated by settle-iters of settling."
+  ([pers] 
+   (settle-analogy-net pers 0))
+  ([pers iters]
+   (settle-net pers :analogy-activns :analogy-net iters)))
+
+(defn settle-propn-net
+  "Return person pers with its propn net updated by settle-iters of settling."
+  ([pers] 
+   (settle-propn-net pers 0))
+  ([pers iters]
+   (settle-net pers :propn-activns :propn-net iters)))
+
+(defn settle-net
+  "Return person pers with the net selected by net-key and activns-key updated
+  by iters rounds of settling."
+  [pers net-key activns-key iters]
+  (assoc pers activns-key
+         (take iters
+               (iterate (partial next-activns (net-key pers))
+                        (activns-key pers)))))
 
 ;; SHOULD I USE add-product here for the inner addition of emuls?
-;; 
-;; NOTE For This way of doing matrix multiplication using (mmul <matrix> <vector>),
+;; NOTE: For This way of doing matrix multiplication using (mmul <matrix> <vector>),
 ;; <vector> is 1D and is treated as a column vector.  This means that the weight
 ;; at index i,j represents the directional link from node j to node i, since j is
 ;; the column (input) index, and i is the row index.  (Doesn't matter for symmetric
@@ -101,62 +108,6 @@
                (emul (mmul (nn/neg-wt-mat net) pos-activns) ; negatively weighted inputs scaled by
                      (emap dist-from-min activns))))))       ;  inputs' distances from -1.
 
-(defn settle-net
-  "Return person pers with the net selected by net-key and activns-key updated
-  by iters rounds of settling."
-  [pers net-key activns-key iters]
-  (assoc pers activns-key
-         (take iters
-               (iterate (partial next-activns (net-key pers))
-                        (activns-key pers)))))
-
-(defn settle-analogy-net
-  "Return person pers with its analogy net updated by settle-iters of settling."
-  ([pers] 
-   (settle-analogy-net pers 0))
-  ([pers iters]
-   (settle-net pers :analogy-activns :analogy-net iters)))
-
-(defn settle-propn-net
-  "Return person pers with its propn net updated by settle-iters of settling."
-  ([pers] 
-   (settle-propn-net pers 0))
-  ([pers iters]
-   (settle-net pers :propn-activns :propn-net iters)))
-
-(defn update-propn-wts-from-analogy-activns
-  "Currently a noop; returns the person unchanged."
-  [pers]
-  pers)
-
-(defn update-person-nets
-  "Perform one tick's updating of the networks of a single person."
-  [pers]
-  (-> pers
-    (settle-analogy-net  settling-iters) ; is this step necessary??
-    (update-propn-wts-from-analogy-activns)
-    (settle-propn-net    settling-iters)))
-
-(defn update-nets
-  "Implements a single timestep's (tick's) worth of network settling and updating of the
-  proposition network from the analogy network.  Returns the population in its new state
-  after these processes have been performed."
-  [persons]
-  (map update-person-nets persons))
-
-;(defn settle-analogy-nets!
-;  "Currently a noop; returns the population unchanged."
-;  [popn]
-;  (assoc popn :members (map settle-analogy-net! (:members popn))))
-;;; SEE NOTE after settle-propn-nets!
-;
-;(defn settle-propn-nets!
-;  "Currently a noop; returns the population unchanged."
-;  [persons]
-;  (map settle-propn-net! persons))
-;;; If this is really only side-effecting, then it could just be done like this:
-;;   (domap settle-propn-net! (:members popn)))
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; scalar functions
 
@@ -176,4 +127,3 @@
   if activn > 0."
   [activn]
   (+ 1 activn)) ; HT1989 p. 313 says: (- activn -1)
-
