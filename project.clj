e67ddea4ff54cc47ad53fe54a98dded14ec0581c
mabrams <mabrams@uab.edu>
2014-04-26 00:06:48 -0500

    deleted old commented-out line

--- project.clj
+++ project.clj
@@ -5,7 +5,6 @@
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :main popco.core.popco ; this is what lein run will look for -main in
   :profiles {:dev {:dependencies [;[slingshot "0.10.3"][org.jblas/jblas "1.2.3"][mars0i/clatrix "0.4.0-SNAPSHOT"]
-                                  ;[net.mikera/core.matrix "0.20.1-SNAPSHOT"]
                                   [net.mikera/core.matrix "0.22.1-SNAPSHOT"]
                                   [net.mikera/vectorz-clj "0.20.1-SNAPSHOT"]
                                   [org.clojure/algo.generic "0.1.1"]
