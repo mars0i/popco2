b24807c9f9ee99c3ee55f1a54f8b5282f921edeb
mars0i <mabrams@uab.edu>
2014-05-12 00:36:52 -0500

    Put back to default version using core.matrix fns

--- src/popco/nn/matrix.clj
+++ src/popco/nn/matrix.clj
@@ -1,6 +1,5 @@
 (ns popco.nn.matrix
-  (require [clojure.core.matrix :as mx]
-           [popco.core.constants :as cn]))
+  (require [clojure.core.matrix :as mx]))
 ;; vectorz only supports Doubles, and ndarray defaults to Doubles.
 ;; If I want something else with ndarray, I need to replace some of
 ;; its functions with other ones.  But for use with vectorz, the
@@ -10,16 +9,5 @@
 ;; the special version designed for using ndarray with other sorts
 ;; of numbers.
 
-(defn zero-vector
-  "Returns a vector of size size, filled with zeros of the same type as +zero+."
-  [size]
-  (let [zv (mx/zero-vector size)]
-    (mx/fill! zv cn/+zero+)
-    zv))
-
-(defn zero-matrix
-  "Returns a matrix with dimensions rows, cols,, filled with zeros of the same type as +zero+."
-  [rows cols]
-  (let [zm (mx/zero-matrix rows cols)]
-    (mx/fill! zm cn/+zero+)
-    zm))
+(def zero-vector mx/zero-vector)
+(def zero-matrix mx/zero-matrix)
