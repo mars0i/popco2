cc252c4103fc1608c7b9dc64409f57fe5da8bc9e
mabrams <mabrams@uab.edu>
2014-05-12 11:09:07 -0500

    Inc'ed popco version, switched to standard core.matrix build

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject popco2 "0.0.1-SNAPSHOT"
+(defproject popco2 "0.0.2-SNAPSHOT"
   :url "https://github.com/mars0i/popco2"
   :license {:name "to be filled in"
             :url "to be filled in"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :main popco.core.popco ; will be run automatically in the repl
   :profiles {:dev {:dependencies [;[slingshot "0.10.3"][org.jblas/jblas "1.2.3"][mars0i/clatrix "0.4.0-SNAPSHOT"]
-                                  ;[net.mikera/core.matrix "0.22.1-SNAPSHOT"]
-                                  [net.mikera/core.matrix "0.22.1-OTHERZEROS"]
+                                  ;[net.mikera/core.matrix "0.22.1-OTHERZEROS"] ; my hack version that allows mmul to return numbers of the same kind as input
+                                  [net.mikera/core.matrix "0.22.1-SNAPSHOT"]
                                   [net.mikera/vectorz-clj "0.20.1-SNAPSHOT"]
                                   [org.clojure/algo.generic "0.1.1"]
                                   [org.clojure/data.csv "0.1.2"]
