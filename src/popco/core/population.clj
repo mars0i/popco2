5ea9f125b1053548003e964c15f6a90acda1129c
mars0i <mabrams@uab.edu>
2014-05-15 23:07:18 -0500

    added alternative def of invert-coll-map

--- src/popco/core/population.clj
+++ src/popco/core/population.clj
@@ -31,10 +31,11 @@
 ;; THERE MUST BE A SIMPLER METHOD THAN THE FOLLOWING ....
 ;; And if I start revising groups during runtime, this might not be fast enough.
 
-(defn split-coll-map
+(defn join-pair-seq
   "Given a map whose values are collections, return a sequence of maps each
   of which has a key from the original map, and one of the members of the 
-  collection that was its value."
+  collection that was its value.  This is essentially a join table of all
+  unique pairs licensed by the original map."
   [m]
   (mapcat 
     (fn [[k vs]]
@@ -47,26 +48,18 @@
   collections are the keys for the current vals' former collections."
   [m]
   (apply merge-with 
-         (comp vec flatten list) ; set instead of vec doesn't work. why?? flatten returns lazy seq.
-         (map st/map-invert 
-              (split-coll-map m))))
-
-(defn invert-coll-map2
-  "Given a map whose values are collections, return a map of the same sort,
-  but in which each val member is now a key, and the members of their val
-  collections are the keys for the current vals' former collections."
-  [m]
-  (apply merge-with 
-         (comp flatten list)
-         (map st/map-invert 
-              (split-coll-map m))))
-
-(defn invert-coll-map4
-  "Given a map whose values are collections, return a map of the same sort,
-  but in which each val member is now a key, and the members of their val
-  collections are the keys for the current vals' former collections."
-  [m]
-  (apply merge-with 
          (comp flatten vector)
          (map st/map-invert 
-              (split-coll-map m))))
+              (join-pair-seq m))))
+
+(defn invert-coll-map2
+  [m]
+  (let [v-elts (set (apply concat (vals m)))
+        init-maps (map #(hash-map % []) v-elts) ; we need maps with empty colls as keys, so conj will work
+        data-maps (for [[k v] m              ; a coll of maps from elts in val colls, to the keys of those colls
+                        elt v-elts           ; actually this does the same thing as join-pair-seq
+                        :when (contains? (set v) elt)] {elt k})] ; (set v) to make contains? work
+    (apply merge-with conj 
+           (concat init-maps
+                   data-maps))))
+
