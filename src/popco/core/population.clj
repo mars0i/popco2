33e3edad0fadc4ecfd1b36402d720e5a4cc4f9a4
mabrams <mabrams@uab.edu>
2014-05-17 12:12:58 -0500

    Rename group-to-persons to groups

--- src/popco/core/population.clj
+++ src/popco/core/population.clj
@@ -6,12 +6,12 @@
 
 (defrecord Population [tick 
                        persons
-                       group-to-persons])
+                       groups])
 
 (ug/add-to-docstr ->Population
   "\n  tick: timestep.
   persons: members of the population at time tick.
-  group-to-persons: map from group id to person ids representing membership.
+  groups: map from group id to person ids representing membership.
   person-to-groups: map from person id to group ids--i.e. what groups am I in?
   person-talk-to-groups: map from person id to groups of persons to whom person talks.")
 
@@ -25,7 +25,7 @@
         person-to-groups (apply hash-map 
                                 (mapcat #(vector (:id %) (:groups %))
                                         members))
-        group-to-persons (ug/invert-coll-map person-to-groups)
-        updated-members (map (partial pers/update-person-talk-to group-to-persons)
+        groups (ug/invert-coll-map person-to-groups)
+        updated-members (map (partial pers/update-person-talk-to groups)
                              members)]
-    (assoc popn :persons updated-members :group-to-persons group-to-persons)))
+    (assoc popn :persons updated-members :groups groups)))
