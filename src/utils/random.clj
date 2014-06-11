75d35fdeccf766f5912f19fda1e7dcbf0a8c71fb
mars0i <mabrams@uab.edu>
2014-06-10 23:20:51 -0500

    experiments

--- src/utils/random.clj
+++ src/utils/random.clj
@@ -5,3 +5,22 @@
   (:import [ec.util MersenneTwister MersenneTwisterFast] ; EXPERIMENTING--NEED TO DEAL WITH LICENSE NOTICES BEFORE RELEASE
            [SFMT19937])) ; EXPERIMENTING--NEED TO DEAL WITH LICENSE NOTICES BEFORE RELEASE
 
+(defn make-rand-idx
+  [rng n]
+  (* n (blahblah)))
+
+;; note these are lazy
+
+(defn make-sample-with-replacement-1
+  [rand-idx num-samples coll]
+  (repeatedly num-samples 
+              #(nth coll (partial rand-idx (count coll))))) ; does repeatedly make this get reevaluated every time?
+
+
+;; This version is inspired by Incanter, which does it like this:
+;;        (map #(nth x %) (sample-uniform size :min 0 :max max-idx :integers true))
+(defn make-sample-with-replacement-2
+  [rand-idx num-samples coll]
+  (map #(nth x %) 
+       (repeatedly num-samples 
+                   (partial rand-idx (count coll))))
