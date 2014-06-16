9b9951e5c7ff8a6944a20ac208be78989ccc86da
mabrams <mabrams@uab.edu>
2014-06-15 21:30:21 -0500

    Revised main loop, reporting to user; added csv write

--- test/samplingtests2.clj
+++ test/samplingtests2.clj
@@ -2,35 +2,64 @@
   (:use [criterium.core :only [bench benchmark]]
         [popco.core.main]
         [utils.random]
-        [sims.crime3.groups2]))
+        [sims.crime3.groups2]
+        [popco.io.propncsv :only [spit-csv]]))
 
-(def ticks 2)
+(def ticks 500)
 
 (def test-results {})
 
-(doseq [rng-fn [make-rng-mt make-rng-mtf make-rng-java]
-        sample-repl-fn [sample-with-repl-1 sample-with-repl-2 sample-with-repl-3 sample-with-repl-4]
-        many-times-fn [many-times unparalleled-many-times]]
-        ;; set up rng's and functions etc. for this run. intern allows redefining in another namespace:
-        (intern 'utils.random 'make-rng rng-fn)
-        (intern 'utils.random 'sample-with-repl sample-repl-fn)
-        (intern 'popco.core.person 'initial-rng (utils.random/make-rng 1400811420068))
-        ;; cause the population to be remade using the current rng and sample fns:
-        (load-file "src/sims/crime3/groups2.clj")
-        ;; generate strings to function as keys in a hash, labels in a spreadsheet:
-        (let [rng-name (utils.general/extract-fn-name rng-fn)
-              sample-repl-name (utils.general/extract-fn-name sample-repl-fn)
-              many-times-name (utils.general/extract-fn-name many-times-fn)]
-          ;; tell user how far along we are, using actual function internal names for extra sanity check:
-          (print "Testing" rng-fn "in" sample-repl-fn "with" many-times-fn ":")
-          ;; run test and store the new result into the results hash:
-          (def test-results 
-            (assoc-in test-results 
-                      [rng-name sample-repl-name many-times-name]
-                      (first (:mean (benchmark 
-                                      (def _ (nth (many-times-fn popn) ticks))
-                                      '())))))
-          (println (get-in test-results [rng-name sample-repl-name many-times-name])))) ; let user know latest mean time
+(def rng-fns [make-rng-mt make-rng-mtf make-rng-java])
+(def rng-names (map utils.general/extract-fn-name rng-fns))
+(def sample-with-repl-fns [sample-with-repl-1 sample-with-repl-2 sample-with-repl-3 sample-with-repl-4])
+(def sample-with-repl-names (map utils.general/extract-fn-name sample-with-repl-fns))
+(def many-times-fns [many-times unparalleled-many-times])
+(def many-times-names (map utils.general/extract-fn-name many-times-fns))
 
-        (println "Results: ")
-        (pprint test-results)
+(doseq [rng-fn rng-fns
+        sample-with-repl-fn sample-with-repl-fns
+        many-times-fn many-times-fns]
+  ;; set up rng's and functions etc. for this run. intern allows redefining in another namespace:
+  (intern 'utils.random 'make-rng rng-fn)
+  (intern 'utils.random 'sample-with-repl sample-with-repl-fn)
+  (intern 'popco.core.person 'initial-rng (utils.random/make-rng 1400811420068))
+  ;; cause the population to be remade using the current rng and sample fns:
+  (load-file "src/sims/crime3/groups2.clj")
+  ;; generate strings to function as keys in a hash, labels in a spreadsheet:
+  (let [rng-name (utils.general/extract-fn-name rng-fn)
+        sample-with-repl-name (utils.general/extract-fn-name sample-with-repl-fn)
+        many-times-name (utils.general/extract-fn-name many-times-fn)]
+    ;; tell user how far along we are, using actual function internal names for extra sanity check:
+    ;(println "\nTesting with")
+    ;(println rng-fn)
+    ;(println sample-with-repl-fn)
+    ;(println many-times-fn)
+    (print rng-name sample-with-repl-name many-times-name ": Mean seconds per call: ")(flush)
+    ;; run test:
+    (let [result (first (:mean (benchmark (def _ (nth (many-times-fn popn) ticks)) '())))] ; produces mean seconds per call
+      ;; let user know what happened:
+      ;(println "Mean time for" rng-name sample-with-repl-name many-times-name ":" result)(flush)
+      (println result)(flush)
+      ;; store the new result into the results hash:
+      (def test-results 
+        (assoc-in test-results 
+                  [rng-name sample-with-repl-name many-times-name]
+                  result)))))
+
+;; TODO not right should be a 2-level sequence, not 3
+;(spit-csv "samplingtests2.csv"
+;          (for [rng-key (keys test-results) :let [sample-fn-map (test-results rng-key)]]
+;            (for [sample-fn-key (keys sample-fn-map) :let [many-times-fn-map (sample-fn-map sample-fn-key)]]
+;              [rng-key sample-fn-key (many-times-fn-map "many-times") (many-times-fn-map "unparalleled-many-times")])))
+
+(spit-csv "samplingtests2.csv"
+          (for [rng-name rng-names
+                sample-with-repl-name sample-with-repl-names]
+            (let [many-times-fn-map (get-in test-results [rng-name sample-with-repl-name])
+                  many-times-result (many-times-fn-map "many-times")
+                 unparalleled-many-times-result (many-times-fn-map "unparalleled-many-times")]
+              [rng-name sample-fn-name many-times-result unparalleled-many-times-result]))) ; last two elements are mean seconds per call with pmap and without pmap
+
+(println "\nResults: ")
+(clojure.pprint/pprint test-results)
+(flush)
