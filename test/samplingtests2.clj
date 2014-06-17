f4b511c5e1d8e3747d8dab083a0b61e960d2e3b9
mabrams <mabrams@uab.edu>
2014-06-17 07:12:40 -0500

    removed unnecessary function name in vector

--- test/samplingtests2.clj
+++ test/samplingtests2.clj
@@ -20,7 +20,7 @@
 (def rng-fns [make-rng-mt make-rng-mtf make-rng-java])
 (def rng-names (map utils.general/extract-fn-name rng-fns)) ; note this converts dashes to underlines
 
-(def sample-with-repl-fns [sample-with-repl-1-dummy sample-with-repl-1 sample-with-repl-1 sample-with-repl-2 sample-with-repl-3 sample-with-repl-4])
+(def sample-with-repl-fns [sample-with-repl-1-dummy sample-with-repl-1 sample-with-repl-2 sample-with-repl-3 sample-with-repl-4])
 (def sample-with-repl-names (map utils.general/extract-fn-name sample-with-repl-fns))
 
 (def many-times-fns [many-times unparalleled-many-times])
