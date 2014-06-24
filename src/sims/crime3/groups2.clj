c5dfd855527fec3f2a7cc8471564e5bfbe2e0076
mars0i <mabrams@uab.edu>
2014-06-24 10:25:56 -0500

    Bugfix: was creating persons with same ids

--- src/sims/crime3/groups2.clj
+++ src/sims/crime3/groups2.clj
@@ -7,42 +7,42 @@
             [popco.core.constants :as cn]
             [sims.crime3.propns :as pns]))
 
-(def propns (concat pns/crime-propns pns/living-propns))  ; ok if lazy; make-* functions will realize it
-(def crime-ids (map :id pns/crime-propns)) ; ok if lazy; make-* functions will realize it
+(let[propns (concat pns/crime-propns pns/living-propns)  ; ok if lazy; make-* functions will realize it
+     crime-ids (map :id pns/crime-propns) ; ok if lazy; make-* functions will realize it
 
-;; PECEPTION-IFS
-;; Directional activation flows from j to i, i.e. here from salient to the crime propn node
-(def perception-ifs (map #(vector cn/+one+ (:id %) :SALIENT) pns/crime-propns)) ; ok if lazy; make-* functions will realize it
+     ;; PECEPTION-IFS
+     ;; Directional activation flows from j to i, i.e. here from salient to the crime propn node
+     perception-ifs (map #(vector cn/+one+ (:id %) :SALIENT) pns/crime-propns) ; ok if lazy; make-* functions will realize it
 
-;; PROPOSITION NET (TEMPLATE FOR INDIVIDUAL NETS)
-(def pnet (pn/make-propn-net propns pns/semantic-iffs perception-ifs)) ; second arg is bidirectional links; third is unidirectional
+     ;; PROPOSITION NET (TEMPLATE FOR INDIVIDUAL NETS)
+     pnet (pn/make-propn-net propns pns/semantic-iffs perception-ifs) ; second arg is bidirectional links; third is unidirectional
 
-;; ANALOGY NET (TO BE SHARED BY EVERYONE)
-(def anet (an/make-analogy-net pns/crime-propns 
+     ;; ANALOGY NET (TO BE SHARED BY EVERYONE)
+     anet (an/make-analogy-net pns/crime-propns 
                                pns/living-propns 
-                               pns/conceptual-relats))
+                               pns/conceptual-relats)
 
-(def c1 (pers/make-person :c1                      ; name
-                          propns                   ; all of the propositions I might come to know
-                          pnet                     ; proposition-network
-                          anet                     ; analogy-network
-                          crime-ids                ; utterable-ids: ids of propns I'm willing to talk about
-                          [:central]               ; groups: what groups I'm in
-                          [:central :west :east]   ; talk-to-groups: groups whose members I'm willing to say something to
-                          1))                      ; max=talk-to: maximum number of people I'm willing to say something to
+     c (pers/make-person :c                      ; name
+                         propns                   ; all of the propositions I might come to know
+                         pnet                     ; proposition-network
+                         anet                     ; analogy-network
+                         crime-ids                ; utterable-ids: ids of propns I'm willing to talk about
+                         [:central]               ; groups: what groups I'm in
+                         [:central :west :east]   ; talk-to-groups: groups whose members I'm willing to say something to
+                         1)                      ; max=talk-to: maximum number of people I'm willing to say something to
 
-(def e1 (pers/make-person :e1 propns pnet anet crime-ids [:east] [:east :west] 2))
+     e (pers/make-person :e propns pnet anet crime-ids [:east] [:east :west] 2)
 
-(def s1 (pers/make-person :s1 propns pnet anet crime-ids [:west :east] [:central] 3)) ;"s1": split
+     s (pers/make-person :s propns pnet anet crime-ids [:west :east] [:central] 3) ;"s": split
 
-(def w1 (pers/make-person :w1 propns pnet anet crime-ids [:west] [:west :east] 4))
+     w (pers/make-person :w propns pnet anet crime-ids [:west] [:west :east] 4)]
 
-(def popn (pp/make-population (vec (concat
-                                     [c1 w1 e1 s1]
-                                     (repeatedly 25 #(pers/new-person-from-old c1 :c2))
-                                     (repeatedly 25 #(pers/new-person-from-old w1 :w2))
-                                     (repeatedly 25 #(pers/new-person-from-old e1 :e2))
-                                     (repeatedly 25 #(pers/new-person-from-old s1 :s2))))))
+  (def popn (pp/make-population (vec (concat
+                                       [c w e s]
+                                       (repeatedly 25 #(pers/new-person-from-old c))
+                                       (repeatedly 25 #(pers/new-person-from-old w))
+                                       (repeatedly 25 #(pers/new-person-from-old e))
+                                       (repeatedly 25 #(pers/new-person-from-old s)))))))
 
-;; Useful in order to see what's1 going on:
+;; Useful in order to see what's going on:
 ; (do (println "\n" (:groups popn) "\n") (domap #(do (println (:id %) (:talk-to-groups %)) (println (:talk-to-persons %) "\n")) (sort-by :id (:persons popn))))
