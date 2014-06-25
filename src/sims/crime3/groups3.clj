93ab282b9e27928a0a328ac5cc971faecc4333fb
mars0i <mabrams@uab.edu>
2014-06-25 12:49:50 -0500

    misc experimental changes

--- src/sims/crime3/groups3.clj
+++ src/sims/crime3/groups3.clj
@@ -6,14 +6,23 @@
             [popco.core.constants :as cn]
             [sims.crime3.propns :as pns]))
 
+;; ************************
+;; TIPS:
+;; - If a person starts with all propns unmasked, the analogy net will never change.
+;; - If the only things uttered are propns that are already perceived, the propn net will never change.
+;; ************************
+
 (let [propns (concat pns/crime-propns pns/living-propns)  ; ok if lazy; make-* functions will realize it
-      crime-ids (map :id pns/crime-propns) ; ok if lazy; make-* functions will realize it
+      crime-ids  (map :id pns/crime-propns) ; ok if lazy; make-* functions will realize it
+      living-ids (map :id pns/living-propns)
 
       ;; PECEPTION-IFS
       ;; Directional activation flows from j to i, i.e. here from salient to the crime propn node
+      ;; ALL AND ONLY CRIME PROPNS ARE PERCEIVED
       perception-ifs (map #(vector cn/+one+ (:id %) :SALIENT) pns/crime-propns) ; ok if lazy; make-* functions will realize it
 
       ;; PROPOSITION NET (TEMPLATE FOR INDIVIDUAL NETS)
+      ;; ALL POSSIBLE PROPNS ARE UNMASKED HERE:
       pnet (pn/make-propn-net propns pns/semantic-iffs perception-ifs) ; second arg is bidirectional links; third is unidirectional
 
       ;; ANALOGY NET (TO BE SHARED BY EVERYONE)
@@ -21,8 +30,9 @@
                                 pns/living-propns 
                                 pns/conceptual-relats)
 
-      e1 (pers/make-person :e1 propns pnet anet crime-ids [:east] [:east :west] 2)
-      w1 (pers/make-person :w1 propns pnet anet crime-ids [:west] [:west :east] 2)]
+      ;; args:              id propns pnet anet utterable-ids mygroups talk-to-groups max-talk-to
+      e1 (pers/make-person :e1 propns pnet anet living-ids    [:east]  [:east :west]  2)
+      w1 (pers/make-person :w1 propns pnet anet living-ids    [:west]  [:east :west]  2)]
 
   (def popn (pp/make-population (vec (concat
                                        [e1 w1]
