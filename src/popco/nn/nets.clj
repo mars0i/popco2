d522f68a27b66351d87ed14c983dabe11bae5070
mabrams <mabrams@uab.edu>
2014-08-04 11:38:33 -0500

    ashow-utterance-salient-effects: round activns: strip float noise

--- src/popco/nn/nets.clj
+++ src/popco/nn/nets.clj
@@ -281,7 +281,7 @@
        (pp/cl-format true "~s: " (:tick curr))
        (pp/pprint (map 
                     #(vector (:id %) 
-                             (map (fn [[[idx] wt]] [(:id (node-vec idx)) wt])
+                             (map (fn [[[idx] wt]] [(:id (node-vec idx)) (ug/round2 3 wt)]) ; round to strip spurious small decimals from float noise
                                   (pmx/non-zeros (salient-wts %))))
                     (drop to-skip (:persons curr))))))))
 
