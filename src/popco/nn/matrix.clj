282563f9bb2f83d6e9fa3c2f180b0498bd7213c5
mabrams <mabrams@uab.edu>
2014-08-25 10:08:37 -0500

    Added new non-zeros defs and row-to-square etc.

--- src/popco/nn/matrix.clj
+++ src/popco/nn/matrix.clj
@@ -2,14 +2,43 @@
   (require [clojure.core.matrix :as mx]))
 ;; matrix utility functions
 
+(def square-from-row
+  "row-mat must be a *matrix* not a vector, i.e. it must be 2D 
+  even though only one row."
+  [row-mat]
+  (mx/mmul (transpose row-mat) row-mat))
+
+(def square-from-col
+  "col-mat must be a *matrix* not a vector, i.e. it must be 2D 
+  even though only one column."
+  [col-mat]
+  (mx/mmul col-mat (transpose col-mat)))
+
+(defn non-zero-index-val-pairs
+  "Return [indices val] pairs for non-zero vals in matrix m.
+  (See docstring for non-zeros.)"
+  [m]
+  (filter (comp not zero? second) 
+          (map vector (mx/index-seq m) (mx/eseq m))))
+
 (defn non-zeros
   "Gets the non-zero indices of an array mapped to the values.
   (By Matt Revelle at https://github.com/mikera/core.matrix/issues/102
   Something like this will probably be incorporated into core.matrix
   with the name non-zero-map.)"
-  ([m] (into {} 
-             (filter (comp not zero? second) 
-                     (map vector (mx/index-seq m) (mx/eseq m))))))
+  [m]
+  (into {}
+        (non-zero-index-val-pairs m)))
+
+(defn non-zero-indices
+  [m]
+  (map first 
+       (non-zero-index-val-pairs m)))
+
+(defn non-zero-vals
+  [m]
+  (map second
+       (non-zero-index-val-pairs m)))
 
 (defn col1
   [m]
