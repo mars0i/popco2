6794f57b0fe3d494ba143c7fc1d20bac78df84fd
mabrams <mabrams@uab.edu>
2014-09-13 14:25:04 -0500

    Added comments

--- src/popco/core/reporters.clj
+++ src/popco/core/reporters.clj
@@ -8,13 +8,20 @@
             [popco.io.propncsv]
             [popco.nn.nets]))
 
+;; MOST of these functions take a single population, generate some
+;; output, and return the population unchanged.  This illustrates
+;; a typical use, where popn contains an intial population:
+;;    (def popns (map ticker (take 100 (many-times popn))))
+
 (def ticker                          popco.core.main/ticker)
 (def dotter                          popco.core.main/dotter)
 
 (def display-utterances              popco.communic.listen/display-utterances)
 
-(def write-propn-activns-csv         popco.io.propncsv/write-propn-activns-csv)
-
 (def display-salient-wts             popco.nn.nets/display-salient-wts)
 (def display-semantic-wts            popco.nn.nets/display-semantic-wts)
 (def show-utterance-salient-effects  popco.nn.nets/show-utterance-salient-effects)
+
+;; THIS FUNCTION DOES NOT RETURN POPULATIONS.
+;; It expects an entire *sequence* of populations, does not return it.
+(def write-propn-activns-csv         popco.io.propncsv/write-propn-activns-csv)
