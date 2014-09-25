e23fe105b0cbe5d051f19853d1c64f2af13512c1
mabrams <mabrams@uab.edu>
2014-09-25 13:03:00 -0500

    Added keyword option :cooker and function to cook names for R

--- src/popco/io/propncsv.clj
+++ src/popco/io/propncsv.clj
@@ -6,6 +6,7 @@
   (:require [popco.core.constants :as cn]
             [clojure.data.csv :as csv]
             [clojure.java.io :as io]
+            [clojure.string :as st]
             [clojure.core.matrix :as mx]))
 
 
@@ -13,7 +14,7 @@
 
 ;; ARE THESE FUNCTIONS EFFICIENT ENOUGH?
 
-(declare column-names person-propn-activns propn-activns-row spit-csv write-propn-activns-csv)
+(declare column-names person-propn-activns propn-activns-row spit-csv write-propn-activns-csv cook-name-for-R)
 
 (defn write-propn-activns-csv
   "Collects reads activns from a sequence of opulations into a large seq of 
@@ -26,10 +27,11 @@
   (when-not (== 0 cn/+feeder-node-idx+) ; sanity check for person-propn-activns
     (throw (Exception. (format "This code assumes SALIENT node index is 0, but it's not."))))
   (let [append? (if options ((apply hash-map options) :append) nil)
+        name-cooker (if options ((apply hash-map options) :cooker) identity)
         data (map propn-activns-row popns) ; pmap wouldn't help, because the popns comes from iterate, so you need earlier elements to get later ones (unless you doall it first)
         rows (if append?
                data 
-               (cons (column-names (first popns)) ; if not appending, add header row
+               (cons (column-names (first popns) name-cooker) ; if not appending, add header row
                      data))]
     (apply spit-csv (str cn/+data-dir+ "/activns" cn/session-id ".csv") rows options))) ; could pass the hashmap to write, but spit-csv is convenient and should require separate args
 
@@ -48,6 +50,13 @@
   (rest   ; strip SALIENT node (see sanity check exception in write-propn-activns-csv)
         (mx/matrix :persistent-vector 
                    (:activns (:propn-net pers)))))
+
+(defn cook-name-for-R
+  [nm]
+  (-> nm
+      (st/replace #"->-" ".p.") ; prevents
+      (st/replace #"->" ".c.")  ; causes
+      (st/replace #"-" "_")))
  
 (defn column-names
   "Given a sequence of persons, return a sequence of strings containing
@@ -55,14 +64,18 @@
   to the front of the proposition id string, with the form \"person_propn\".
   These are suitable for use as column names in a csv file containing data
   on proposition activations for all of the persons.  Note that the number
-  of strings returned will be (number of persons X number of propositions)."
-  [popn]
-  (let [persons (:persons popn)
-        name-strs (map (comp name :id) persons)
-        id-strs (map name (rest (:id-vec (:propn-net (first persons)))))]
-    (for [name-str name-strs
-          id-str id-strs]
-      (str name-str "_" id-str))))
+  of strings returned will be (number of persons X number of propositions).
+  If name-cooker is present, it should be a function that will modify column
+  name strings.  (Example: The function cook-name-for-R will perform 
+  replacements to make column names readable in the R language.)"
+  ([popn] (column-names popn identity))
+  ([popn name-cooker]
+   (let [persons (:persons popn)
+         name-strs (map (comp name :id) persons)
+         id-strs (map name (rest (:id-vec (:propn-net (first persons)))))]
+     (for [name-str name-strs
+           id-str id-strs]
+       (name-cooker (str name-str "_" id-str))))))
 
 (defn spit-csv
   "Given a sequence of sequences of data, opens a file and writes to it
