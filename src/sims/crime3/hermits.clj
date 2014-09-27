d742807537f90b734d30852ca2d07222fffcdf1e
mabrams <mabrams@uab.edu>
2014-09-27 15:08:52 -0500

    New model: 4 persons, no talking, diff perceptions

---
+++ src/sims/crime3/hermits.clj
@@ -0,0 +1,20 @@
+;;; This software is copyright 2013, 2014, 2015 by Marshall Abrams, and
+;;; is distributed under the Gnu General Public License version 3.0 as
+;;; specified in the file LICENSE.
+
+
+;; SIMPLE TEST OF WHETHER ANALOGIES WORKING AS INTENDED
+;; No communication.  Everyone has all propositions, but differ in what they perceive.
+(ns sims.crime3.hermits
+  (:require [popco.core.person :as pers]
+            [popco.core.population :as pp]
+            [sims.crime3.propns :as p]))
+
+
+      ;; args:                 ID      UNMASKED          PROPN-NET                 ANALOGY-NET  UTTERABLE-IDS        GROUPS            TALK-TO-GROUPS        MAX-TALK-TO
+      ;;                            (propns entertained) (propns perceived)                   (propns I can say)   (Groups I'm in)   (Groups I talk to)    (Max number of people I talk to in one tick)
+(let [bea    (pers/make-person :bea    p/all-propns      p/beast-perc-pnet         p/anet       []                   []                []                    0)
+      becky  (pers/make-person :becky  p/all-propns      p/beastly-crime-perc-pnet p/anet       []                   []                []                    0)
+      vicky  (pers/make-person :vicky  p/all-propns      p/viral-crime-perc-pnet   p/anet       []                   []                []                    0)
+      virgil (pers/make-person :virgil p/all-propns      p/virus-perc-pnet         p/anet       []                   []                []                    0)]
+  (def popn (pp/make-population [bea becky vicky virgil])))
