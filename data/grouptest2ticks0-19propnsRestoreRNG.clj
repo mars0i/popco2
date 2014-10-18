e58b238039723a0a22339fde400eb7df53c15818
mabrams <mabrams@uab.edu>
2014-10-18 13:55:49 -0500

    Trying to reorganize long-term data

--- data/grouptest2ticks0-19propnsRestoreRNG.clj
+++
@@ -1,4 +0,0 @@
-(intern 'popco.core.constants 'session-id 1409511793094)
-(println "Session id/seed:" popco.core.constants/session-id)
-(intern 'popco.core.constants 'initial-rng (utils.random/make-rng popco.core.constants/session-id))
-
