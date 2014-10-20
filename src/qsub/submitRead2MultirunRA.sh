14e6d5c3d9a29fe4d84c000e3fc06960b9453e4b
Marshall Abrams <mabrams@uab.edu>
2014-10-20 11:12:23 -0500

    Modified qsub scripts.

--- src/qsub/submitRead2MultirunRA.sh
+++ src/qsub/submitRead2MultirunRA.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-csvvec=`echo *.csv | files2vec`
+csvvec=`echo *.csv | ~/p2/src/R/shell/files2vec`
 if [ -z "$csvvec" -o -z "$1" ]; then
 	echo usage: $0 mra
 	echo Note: Should be run from the directory containing the csvs you want to process.
