6e60d27dce5090498b6d92a131fc323295de30e8
mabrams <mabrams@uab.edu>
2014-10-22 10:24:43 -0500

    added source line

--- src/R/examples/crimeRA2DF.R
+++ src/R/examples/crimeRA2DF.R
@@ -1,5 +1,6 @@
-# crimeRA2DF.R
+# R --no-save
 
+source("~/p2/src/R/R/df2ra.R")
 load("crime3.rdata")
 crime3.df <- multiRAs2combinedMeanDF(list(crime3[2:41,,,], crime3[42:81,,,], crime3[82:121,,,]), c("virus", "beast", "both"), "CV", "CB")
 save(crime3.df, file="crime3df.rdata")
