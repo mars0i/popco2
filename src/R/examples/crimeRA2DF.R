f747cb2b505fc5fa0b02d72f685c4be7ae894ae9
mabrams <mabrams@uab.edu>
2014-10-22 10:21:38 -0500

    simple script to create dataframe from multi-run array

---
+++ src/R/examples/crimeRA2DF.R
@@ -0,0 +1,5 @@
+# crimeRA2DF.R
+
+load("crime3.rdata")
+crime3.df <- multiRAs2combinedMeanDF(list(crime3[2:41,,,], crime3[42:81,,,], crime3[82:121,,,]), c("virus", "beast", "both"), "CV", "CB")
+save(crime3.df, file="crime3df.rdata")
