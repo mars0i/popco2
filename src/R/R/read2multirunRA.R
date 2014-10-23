3009d3c5f32cf0a9d2797b6d21a051e5938eb6e6
mars0i <mabrams@uab.edu>
2014-10-22 23:05:44 -0500

    script to run read2multirunRA in batch

---
+++ src/R/R/read2multirunRA.R
@@ -0,0 +1,14 @@
+# read2multirunRA.R
+
+# qsub submitanything.job Rscript --no-init-file --verbose read2multirunRA.R
+
+args <- commandArgs(TRUE)
+
+arrayname <- args[1]
+filename <- paste0(arrayname, ".rdata")
+
+csvvec <- args[-1]
+
+source("~/p2/src/R/R/df2ra.R")
+assign(arrayname, read2multirunRA(csvvec))
+save(list=arrayname, file=filename)
