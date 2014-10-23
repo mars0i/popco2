7ed5b2d675963705cf2f78325939695395fecaef
mars0i <mabrams@uab.edu>
2014-10-22 23:05:59 -0500

    comments

--- src/R/examples/crimeFindDisagreement.R
+++ src/R/examples/crimeFindDisagreement.R
@@ -1,5 +1,8 @@
 # crimeFindDisagreement.R
 
+# run e.g. as:
+# qsub submitanything.job Rscript --no-init-file --verbose crimeFindDisagreement.R
+
 source("~/p2/src/R/R/df2ra.R")
 load("crime3.rdata")
 print(findRunsWithDisagreement(mra[2:41,,,], 0.01))
--- src/R/examples/crimeRA2df.R
+++ src/R/examples/crimeRA2df.R
@@ -1,7 +1,7 @@
 # crimeRA2df.R
 
 # run e.g. as:
-# qsub submitanything.job R --no-save crimeRA2DF.R
+# qsub submitanything.job Rscript --no-init-file --verbose crimeRA2DF.R
 
 source("~/p2/src/R/R/df2ra.R")
 load("crime3.rdata")
