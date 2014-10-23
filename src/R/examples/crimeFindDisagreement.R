151fc4d9bbb495957faee3f3af549632e584816a
Marshall Abrams <mabrams@uab.edu>
2014-10-23 10:38:32 -0500

    bugfix replace "mra" with "crime3"

--- src/R/examples/crimeFindDisagreement.R
+++ src/R/examples/crimeFindDisagreement.R
@@ -5,6 +5,6 @@
 
 source("~/p2/src/R/R/df2ra.R")
 load("crime3.rdata")
-print(findRunsWithDisagreement(mra[2:41,,,], 0.01))
-print(findRunsWithDisagreement(mra[42:81,,,], 0.01))
-print(findRunsWithDisagreement(mra[82:121,,,], 0.01))
+print(findRunsWithDisagreement(crime3[2:41,,,], 0.01))
+print(findRunsWithDisagreement(crime3[42:81,,,], 0.01))
+print(findRunsWithDisagreement(crime3[82:121,,,], 0.01))
