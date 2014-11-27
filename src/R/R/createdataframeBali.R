a1d9666da5d40a49cf4a4cfb154aa10ec2b25156
Marshall Abrams <mabrams@uab.edu>
2014-11-26 19:17:38 -0600

    added gc() calls

--- src/R/R/createdataframeBali.R
+++ src/R/R/createdataframeBali.R
@@ -1,4 +1,7 @@
 source("~/p2/src/R/R/df2ra.R")
+gc()
 load("bali.rdata") # from sims/bali/threegroups.clj, i.e. test differeing influence of worldly alternatives on spiritual beliefs
+gc()
 bali.df <- multiRAs2combinedMeanDF(list(bali[2:41,,,], bali[42:81,,,], bali[82:121,,,]), c("worldly brahman", "worldly peasant", "both"), "SB", "SP")
+gc()
 save(bali.df, file="balidf.rdata")
