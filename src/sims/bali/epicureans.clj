cafc40d551ab5d8417085367e33774c9e81f5208
mars0i <mabrams@uab.edu>
2014-12-02 23:58:07 -0600

    cleanup

--- src/sims/bali/epicureans.clj
+++ src/sims/bali/epicureans.clj
@@ -9,9 +9,9 @@
 
 ;; One kind of person, other than the pundit: An "Epicurean god"--someone who knows of the spiritual, and neither cares nor knows about anything worldly.
 
-;; args:                        ID  UNMASKED            PROPN-NET             ANALOGY-NET UTTERABLE-IDS          GROUPS            TALK-TO-GROUPS     MAX-TALK-TO
-aa     (pers/make-person :aa  p/spiritual-propns  p/spiritual-perc-pnet p/anet      p/spiritual-propn-ids  [:pundits]        [:epicurean-gods]  1)
-nina   (pers/make-person :nina  p/spiritual-propns  p/no-perc-pnet        p/anet      p/spiritual-propn-ids  [:epicurean-gods] [:epicurean-gods]  1)]
+;; args:                        ID  UNMASKED        PROPN-NET             ANALOGY-NET UTTERABLE-IDS          GROUPS            TALK-TO-GROUPS     MAX-TALK-TO
+aa     (pers/make-person :aa    p/spiritual-propns  p/spiritual-perc-pnet p/anet      p/spiritual-propn-ids  [:pundits]        [:epicurean-gods]  1)
+nina   (pers/make-person :nina  p/spiritual-propns  p/no-perc-pnet        p/anet      p/spiritual-propn-ids  [:epicurean-gods] [:epicurean-gods]  1)
 
 (def popn (pp/make-population (vec
                                 (concat
