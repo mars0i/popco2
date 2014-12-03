9caa9e5aa0b57989989103cfc688eb4f6589f398
mars0i <mabrams@uab.edu>
2014-12-02 23:50:53 -0600

    got rid of unnecessary let

--- src/sims/bali/epicureans.clj
+++ src/sims/bali/epicureans.clj
@@ -9,13 +9,11 @@
 
 ;; One kind of person, other than the pundit: An "Epicurean god"--someone who knows of the spiritual, and neither cares nor knows about anything worldly.
 
-(let [spiritual+worldly-brahmanic-propns (concat p/spiritual-propns p/worldly-brahmanic-propns)
-      spiritual+worldly-peasant-propns   (concat p/spiritual-propns p/worldly-peasant-propns)
 ;; args:                        ID  UNMASKED            PROPN-NET             ANALOGY-NET UTTERABLE-IDS          GROUPS            TALK-TO-GROUPS     MAX-TALK-TO
-      aa     (pers/make-person :aa  p/spiritual-propns  p/spiritual-perc-pnet p/anet      p/spiritual-propn-ids  [:pundits]        [:epicurean-gods]  1)
-      nina   (pers/make-person :nina  p/spiritual-propns  p/no-perc-pnet        p/anet      p/spiritual-propn-ids  [:epicurean-gods] [:epicurean-gods]  1)]
-      
-  (def popn (pp/make-population (vec
-                                  (concat
-                                    [aa]
-                                    (take 40 (pers/new-person-seq-from-old nina)))))))
+aa     (pers/make-person :aa  p/spiritual-propns  p/spiritual-perc-pnet p/anet      p/spiritual-propn-ids  [:pundits]        [:epicurean-gods]  1)
+nina   (pers/make-person :nina  p/spiritual-propns  p/no-perc-pnet        p/anet      p/spiritual-propn-ids  [:epicurean-gods] [:epicurean-gods]  1)]
+
+(def popn (pp/make-population (vec
+                                (concat
+                                  [aa]
+                                  (take 40 (pers/new-person-seq-from-old nina))))))
