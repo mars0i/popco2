f4cf846a2e7a702b655ebc7db699929af6ec6803
mars0i <mabrams@uab.edu>
2014-12-03 22:51:35 -0600

    Added naysayer

--- src/sims/bali/epicureans.clj
+++ src/sims/bali/epicureans.clj
@@ -9,11 +9,12 @@
 
 ;; One kind of person, other than the pundit: An "Epicurean god"--someone who knows of the spiritual, and neither cares nor knows about anything worldly.
 
-;; args:                        ID  UNMASKED        PROPN-NET             ANALOGY-NET UTTERABLE-IDS          GROUPS            TALK-TO-GROUPS     MAX-TALK-TO
-aa     (pers/make-person :aa    c/spiritual-propns  c/spiritual-perc-pnet c/anet      c/spiritual-propn-ids  [:pundits]        [:epicurean-gods]  1)
-nina   (pers/make-person :nina  c/spiritual-propns  c/no-perc-pnet        c/anet      c/spiritual-propn-ids  [:epicurean-gods] [:epicurean-gods]  1)
+;; args:               ID    UNMASKED           PROPN-NET                 ANALOGY-NET UTTERABLE-IDS          GROUPS            TALK-TO-GROUPS     MAX-TALK-TO
+(let [aat  (pers/make-person :aat  c/spiritual-propns c/spiritual-perc-pnet     c/anet      c/spiritual-propn-ids  [:pundits]        [:epicurean-gods]  1)
+      aaf  (pers/make-person :aaf  c/spiritual-propns c/spiritual-neg-perc-pnet c/anet      c/spiritual-propn-ids  [:pundits]        [:epicurean-gods]  1)
+      nina (pers/make-person :nina c/spiritual-propns c/no-perc-pnet            c/anet      c/spiritual-propn-ids  [:epicurean-gods] [:epicurean-gods]  1)]
 
-(def popn (pp/make-population (vec
-                                (concat
-                                  [aa]
-                                  (take 40 (pers/new-person-seq-from-old nina))))))
+  (def popn (pp/make-population (vec
+                                  (concat
+                                    [aat aaf]
+                                    (take 40 (pers/new-person-seq-from-old nina)))))))
