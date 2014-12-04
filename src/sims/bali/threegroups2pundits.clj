af95d4aadb4f66c9cb2594ee18351154ef7525e1
mars0i <mabrams@uab.edu>
2014-12-03 22:27:53 -0600

    like threegroups but with a naysayer as well as a true believer

---
+++ src/sims/bali/threegroups2pundits.clj
@@ -0,0 +1,24 @@
+;;; This software is copyright 2013, 2014, 2015 by Marshall Abrams, and
+;;; is distributed under the Gnu General Public License version 3.0 as
+;;; specified in the file LICENSE.
+
+(ns sims.bali.threegroups2pundits
+  (:require [popco.core.person :as pers]
+            [popco.core.population :as pp]
+            [sims.bali.collections :as c]))
+
+(let [spiritual+worldly-brahmanic-propns (concat c/spiritual-propns c/worldly-brahmanic-propns)
+      spiritual+worldly-peasant-propns   (concat c/spiritual-propns c/worldly-peasant-propns)
+;; args:                        ID    UNMASKED                           PROPN-NET                 ANALOGY-NET UTTERABLE-IDS          GROUPS      TALK-TO-GROUPS                  MAX-TALK-TO
+      aat     (pers/make-person :aat  c/spiritual-propns                 c/spiritual-perc-pnet     c/anet      c/spiritual-propn-ids  [:pundits]  [:brahmans :peasants :bothans]  1)
+      aaf     (pers/make-person :aaf  c/spiritual-propns                 c/spiritual-neg-perc-pnet c/anet      c/spiritual-propn-ids  [:pundits]  [:brahmans :peasants :bothans]  1)
+      brahman (pers/make-person :brah spiritual+worldly-brahmanic-propns c/no-perc-pnet            c/anet      c/spiritual-propn-ids  [:brahmans] [:brahmans]                     1) 
+      both    (pers/make-person :both c/all-propns                       c/no-perc-pnet            c/anet      c/spiritual-propn-ids  [:bothans]  [:bothans]                      1)
+      peasant (pers/make-person :peas spiritual+worldly-peasant-propns   c/no-perc-pnet            c/anet      c/spiritual-propn-ids  [:peasants] [:peasants]                     1)] 
+      
+  (def popn (pp/make-population (vec
+                                  (concat
+                                    [aat aaf]
+                                    (take 40 (pers/new-person-seq-from-old brahman))
+                                    (take 40 (pers/new-person-seq-from-old both))
+                                    (take 40 (pers/new-person-seq-from-old peasant)))))))
