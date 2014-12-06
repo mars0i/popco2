d9d2c20ae28af4cc8347c32ec06138c9a2198508
mabrams <mabrams@uab.edu>
2014-12-06 13:24:19 -0600

    This is now the 2-pundit no bias model

---
+++ src/sims/bali/nobias2pundits.clj
@@ -0,0 +1,21 @@
+;;; This software is copyright 2014, 2015 by Marshall Abrams, and
+;;; is distributed under the Gnu General Public License version 3.0 as
+;;; specified in the file LICENSE.
+
+;; Formerly sims.bali.epicureans
+(ns sims.bali.nobias2pundits
+  (:require [popco.core.person :as pers]
+            [popco.core.population :as pp]
+            [sims.bali.collections :as c]))
+
+;; One kind of person, other than the pundit: An "Epicurean god"--someone who knows of the spiritual, and neither cares nor knows about anything worldly.
+
+;; args:                     ID    UNMASKED           PROPN-NET                 ANALOGY-NET UTTERABLE-IDS          GROUPS            TALK-TO-GROUPS     MAX-TALK-TO
+(let [aat  (pers/make-person :aat  c/spiritual-propns c/spiritual-perc-pnet     c/anet      c/spiritual-propn-ids  [:pundits]        [:epicurean-gods]  1)
+      aaf  (pers/make-person :aaf  c/spiritual-propns c/spiritual-neg-perc-pnet c/anet      c/spiritual-propn-ids  [:pundits]        [:epicurean-gods]  1)
+      nina (pers/make-person :nina c/spiritual-propns c/no-perc-pnet            c/anet      c/spiritual-propn-ids  [:epicurean-gods] [:epicurean-gods]  1)]
+
+  (def popn (pp/make-population (vec
+                                  (concat
+                                    [aat aaf]
+                                    (take 40 (pers/new-person-seq-from-old nina)))))))
