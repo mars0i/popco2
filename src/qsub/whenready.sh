46f584991ff370fe17f5ccefe0ca7cfceddaee70
Marshall Abrams <mabrams@uab.edu>
2014-12-09 22:33:04 -0600

    new generic script to wait for a qsub job and then submit an R script

---
+++ src/qsub/whenready.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if [ -z "$2" ]; then
+	echo usage: `basename $0` jobid R-script
+	echo "checks every 10 minutes to see if qstat still shows job jobid, and"
+	echo "runs R-script when it's done."
+	exit 1
+fi
+
+# wait until first job is done, checking every 10 minutes (hope for no error)
+while [ -n "`qstat | grep $1`" ] ; do sleep 10m ; done
+
+# job is done
+
+if [ -f bali.rdata ]; then
+	cd ~/data
+	qsub -l h_rt=18:00:00,vf=40G -N createdf ~/p2/src/qsub/submitanything.job Rscript --no-init-file --verbose ~/p2/src/R/R/$2
+else
+	echo `basename $0`: datafile missing
+	exit 2
+fi
