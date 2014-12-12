f8e006bc3faea4cc2fa57bde1bcb117afa3538af
mars0i <mabrams@uab.edu>
2014-12-11 22:27:27 -0600

    new version of success/noise-supression functions

--- src/sims/bali/functions.clj
+++ src/sims/bali/functions.clj
@@ -4,33 +4,57 @@
 
 (ns sims.bali.functions
   (:require [sims.bali.collections :as bc]
-            [clojure.core.matrix :as mx]))
+            [clojure.core.matrix :as mx]
+            [utils.random :as ran]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Extra functions for Bali sims
 
-(def idxs 
-  (sort 
-    (map (:id-to-idx bc/no-perc-pnet)
-         bc/spiritual-peasant-propn-ids)))
-
-(def idx-start (first idxs))
-(def idx-end (inc (last idxs)))
-(def idx-len (count idxs))
-
-(when-not (= idxs (range start end))
-  (throw (Exception. "Indexes for these propositions are not sequential.")))
-
+;; utility function
 (defn clip-to-prob
+  "Restict x to the probability range: Returns 0 if x < 0, 1 if x > 1,
+  and x otherwise."
   [x]
   (max 0.0 (min 1.0 x)))
 
+;; Define function that returns the overall influence of spiritual-peasant propositions.
+;; This will be a function of the mean of their activations.
+;; For the Bali sim, this will 
+
+;; Define start, end, len from continuous sequence of indexes of activations the spiritual-peasant propositions:
+(let [idxs (sort 
+             (map (:id-to-idx bc/no-perc-pnet)
+                  bc/spiritual-peasant-propn-ids))]
+  (def ^:const +idxs-len+ (count idxs))
+  (def ^:const +idxs-start+ (first idxs))
+  (def ^:const +idxs-end+ (inc (last idxs)))
+  ;; sanity check:
+  (when-not (= idxs (range +idxs-start+ +idxs-end+))
+    (throw (Exception. "Indexes for these propositions are not sequential."))))
+
+(defn sp-subvector-mean
+  "Returns the mean of subvector of pers's activation vector.  The subvector
+  is defined by constants in the source file."
+  [pers]
+  (let [activns (:activns (:propn-net pers))]
+    (/ (mx/esum (mx/subvector activns +idxs-start+ +idxs-end+))
+       +idxs-len+)))
+
+(defn make-spiritual-peasant-ness
+  "Return a function of the mean of a subvector of activations defined
+  by sp-subvector-mean."
+  [scaling-fn pers]
+  (scaling-fn (sp-subvector-mean pers)))
+
+(def spiritual-peasant-ness (partial make-spiritual-peasant-ness identity))
+
+;; When NetLogo is involved, these will be different, 
+;; but for a popco-only system we close the loop.
+;; See FakeNetLogo.md or files referenced there.
+(def success spiritual-peasant-ness)
+
 (defn worth-saying
   [pers abs-activn]
-  (let [activns (:activns (:propn-net pers))
-        dom-mean (/ (mx/esum 
-                      (mx/subvector activns idx-start idx-end))
-                    idx-len)
-        prob (clip-to-prob (+ abs-activn dom-mean))] ; ???? TODO
+  (let [prob (+ abs-activn (success pers))]
     (< (ran/next-double (:rng pers)) 
        prob)))
