af9837093d3c2606f6ed2112b4da655ef79c2eec
mabrams <mabrams@uab.edu>
2014-12-13 15:57:16 -0600

    Added optional model-quality field to Utterance.

--- src/popco/communic/utterance.clj
+++ src/popco/communic/utterance.clj
@@ -6,15 +6,21 @@
   (:require [clojure.core.matrix :as mx]
             [utils.general :as ug]))
 
-(defrecord Utterance [propn-id valence speaker-id])
+(defrecord Utterance [propn-id valence speaker-id speaker-quality])
 
 (ug/add-to-docstr ->Utterance
   "Make an utterance to be communicated to a listener.
-  :propn-id -   id for propn being communicated.
-  :valence -    Effect of utterance on speaker is proportional to this.
-                Originally, 1 or -1, but other values in [-1,1] are possible.
-  :speaker-id - id of person uttering the proposition.  Has no effect, but
-                provides useful information for someone observing the program.")
+  :propn-id -        id for propn being communicated.
+  :valence -         Effect of utterance on speaker is proportional to this.
+                     Originally, 1 or -1, but other values in [-1,1] are possible.
+  :speaker-id -      id of person uttering the proposition.  Has no effect, but
+                     provides useful information for someone observing the program.
+  :speaker-quality - nil, or a value representing a property for model-bias comparisons
+                     on behalf of the listener in communic/listener.  e.g. this field's
+                     value could represent success, skill, prestige, or a property to be
+                     used for similarity comparisons.  For more complex bias calculations,
+                     such as a flexible similarity calculation, the whole person could be
+                     passed in this field.")
 
 (defn utterance-valence
   [speaker propn-id]
@@ -24,8 +30,10 @@
     (ug/sign-of (mx/mget propn-activns (id-to-idx propn-id)))))
     
 (defn make-utterance
-  [speaker propn-id]
-  (->Utterance propn-id 
-               (utterance-valence speaker propn-id)
-               (:id speaker)))
+  ([speaker propn-id] (make-utterance speaker propn-id nil))
+  ([speaker propn-id speaker-quality]
+   (->Utterance propn-id 
+                (utterance-valence speaker propn-id)
+                (:id speaker)
+                speaker-quality)))
 
