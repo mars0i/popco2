acef09570f872a0608ecb4da53e1e008f85a70e4
mabrams <mabrams@uab.edu>
2014-12-20 12:01:23 -0600

    model WP bias with random success
    The effect should be like models without any success bias, but in those
    models, it was possible for a speaker to incorporate more than one
    utterance in a tick, whereas in this case, only one utterance is
    selected for incorporation on any tick.

---
+++ src/sims/bali/randomWPbias.clj
@@ -0,0 +1,30 @@
+;;; This software is copyright 2013, 2014, 2015 by Marshall Abrams, and
+;;; is distributed under the Gnu General Public License version 3.0 as
+;;; specified in the file LICENSE.
+
+;; Model with worldly-peasant (WP) bias and (1-winner) success bias, but where "success" is randomly chosen.
+;; 2 pundits, each with constant maximum quality.
+(ns sims.bali.randomWPbias
+  (:require [popco.core.person :as pers]
+            [popco.core.population :as pp]
+            [popco.communic.listen :as cl]
+            [sims.bali.collections :as c]
+            [sims.bali.success :as s]))
+
+(let [;spiritual+worldly-brahmanic-propns (concat c/spiritual-propns c/worldly-brahmanic-propns)
+      spiritual+worldly-peasant-propns   (concat c/spiritual-propns c/worldly-peasant-propns)
+;; args:                        ID    UNMASKED                           PROPN-NET                 ANALOGY-NET UTTERABLE-IDS          GROUPS      TALK-TO-GROUPS             MAX-TALK-TO BIAS-FILTER           QUALITY-FN
+      aat     (pers/make-person :aat  c/spiritual-propns                 c/spiritual-perc-pnet     c/anet      c/spiritual-propn-ids  [:pundits]  [:brahmans :peasants :bothans]  1      nil                   (constantly 1))
+      aaf     (pers/make-person :aaf  c/spiritual-propns                 c/spiritual-neg-perc-pnet c/anet      c/spiritual-propn-ids  [:pundits]  [:brahmans :peasants :bothans]  1      nil                   (constantly 1))
+     ;brahman (pers/make-person :brah spiritual+worldly-brahmanic-propns c/no-perc-pnet            c/anet      c/spiritual-propn-ids  [:brahmans] [:brahmans]                     1) 
+      peasant (pers/make-person :peas spiritual+worldly-peasant-propns   c/no-perc-pnet            c/anet      c/spiritual-propn-ids  [:peasants] [:peasants]                     5      cl/max-quality-filter s/random-quality)
+     ;both    (pers/make-person :both c/all-propns                       c/no-perc-pnet            c/anet      c/spiritual-propn-ids  [:bothans]  [:bothans]                      1)
+     ] 
+      
+  (def popn (pp/make-population (vec
+                                  (concat
+                                    [aat aaf]
+                                    ;(take 50 (pers/new-person-seq-from-old brahman))
+                                    (take 50 (pers/new-person-seq-from-old peasant))
+                                    ;(take 50 (pers/new-person-seq-from-old both))
+                                   )))))
