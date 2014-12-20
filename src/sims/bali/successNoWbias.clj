551cb45736114340f4dc30e03010aa363f2b685a
mabrams <mabrams@uab.edu>
2014-12-20 14:54:09 -0600

    comment/formatting

--- src/sims/bali/successNoWbias.clj
+++ src/sims/bali/successNoWbias.clj
@@ -2,7 +2,7 @@
 ;;; is distributed under the Gnu General Public License version 3.0 as
 ;;; specified in the file LICENSE.
 
-;; Model with no worldly-peasant (WP) bias and (1-winner) success bias.
+;; Model with no analogy bias and (1-winner) success bias.
 ;; 2 pundits, each with constant maximum quality.
 (ns sims.bali.successNoWbias
   (:require [popco.core.person :as pers]
@@ -16,7 +16,7 @@
 ;; args:                        ID    UNMASKED                           PROPN-NET                 ANALOGY-NET UTTERABLE-IDS          GROUPS      TALK-TO-GROUPS             MAX-TALK-TO BIAS-FILTER           QUALITY-FN
       aat     (pers/make-person :aat  c/spiritual-propns                 c/spiritual-perc-pnet     c/anet      c/spiritual-propn-ids  [:pundits]  [:brahmans :peasants :bothans]  1      nil                   (constantly 1))
       aaf     (pers/make-person :aaf  c/spiritual-propns                 c/spiritual-neg-perc-pnet c/anet      c/spiritual-propn-ids  [:pundits]  [:brahmans :peasants :bothans]  1      nil                   (constantly 1))
-      peasant (pers/make-person :peas c/spiritual-propns                   c/no-perc-pnet            c/anet      c/spiritual-propn-ids  [:peasants] [:peasants]                     5      cl/max-quality-filter s/success)
+      peasant (pers/make-person :peas c/spiritual-propns                 c/no-perc-pnet            c/anet      c/spiritual-propn-ids  [:peasants] [:peasants]                     5      cl/max-quality-filter s/success)
      ] 
       
   (def popn (pp/make-population (vec
