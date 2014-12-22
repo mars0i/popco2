39834d17cf7a65a9db0c4c2a8b20a07b0c9e8a12
mabrams <mabrams@uab.edu>
2014-12-22 12:03:12 -0600

    added comment

--- src/sims/bali/trust025.clj
+++ src/sims/bali/trust025.clj
@@ -5,6 +5,8 @@
 ;; Redefine popco.core.constants/+trust+ to 0.025 (vs 0.05).
 (ns sims.bali.trust025)
 
+;; Override definition of trust in popco.core.constants.
+;; Why it's OK to do this during initialization, and what to watch out for: http://stackoverflow.com/questions/5181367/is-defn-thread-safe .
 (in-ns 'popco.core.constants)
 (def trust 0.025)
 (in-ns 'sims.bali.trust025)
