6ffd5b0f914e89136c98ec0ef1bc87aeeb70b13d
mabrams <mabrams@uab.edu>
2015-04-19 11:54:04 -0500

    deleted old commented-out code, comments from propns.clj
    copied old version to old/propns_extra_nodes_and_propns.clj

--- src/sims/bali/old/propns_extra_notes_and_propns.clj
+++ src/sims/bali/old/propns_extra_notes_and_propns.clj
@@ -2,16 +2,17 @@
 ;;; is distributed under the Gnu General Public License version 3.0 as
 ;;; specified in the file LICENSE.
 
-;;; PROPOSITIONS FOR BALI ANALOGIES BASED ON STEPHEN LANSING's publications,
-;;; especially _Perfect Order_ (e.g. p. 150), along with works by other authors
-;;; such as Clifford Geertz and Hildred Geertz.  See doc/sims/bali for documentation,
-;;; including notes on motivation for this representation in initialnts*.md.
-;;; see src/sims/bali/old for earlier experiments, including notes about choices.
-
 (ns sims.bali.propns
   (:use popco.core.lot)
   (:require [popco.nn.propn :as pn]
             [popco.nn.analogy :as an]))
+
+;;; PROPOSITIONS FOR BALI ANALOGIES BASED ON STEPHEN LANSING's publications,
+;;; especially _Perfect Order_ (e.g. p. 150), along with works by other authors
+;;; such as Clifford Geertz and Hildred Geertz.  See
+;;; doc/sims/bali for documentation, including notes on motivation for 
+;;; this representation in initialnts*.md.
+
 
 ;; PROPOSITION NAMING CONVENTIONS:
 ;; Brahmanic propns start with "B".
@@ -19,6 +20,37 @@
 ;; Spiritual propns start with "BS" or "PS".
 ;; Worldly propns start with "BW" or "PW".
 ;; Propns shared between Brahmanic and peasant domains start with "XS" or "XW".
+
+
+;; YO: These propns get zero activation when they are part of the contrary analogue.  Should be negative, possibly.
+;; Well, no, it's OK.  crime3 didn't have these isolated subnets, maybe, but sanday definitely did.
+;; And trying to get rid of them is difficult given that I'm using the Grossberg algorithm on the proposition net,
+;; since it means that if you create a semantic link that pushes one member of the net to negative, it will
+;; have no effect on any other member of the subnet--the Grossberg algorithm doesn't transmit negative activations.
+;;
+;; In sophie (spiritual peasant pegged) and wilfred (worldly peasant pegged),
+;; these 4 propns are IN THEIR OWN ISOLATED SUBNET:
+;;  :WB-state-fail-enemy->disorder
+;;  :WB-king-succeed-enemy->order
+;;  :SB-king-fail-demon->disorder
+;;  :SB-state-succeed-demon->order
+;; But e.g. in wilbur, the two WB propns are connected to SALIENT.
+;;
+;; In siobhan (spiritual brahman) and wilbur (worldly brahman), these 10 propns are in three isolated subnets:
+;; 
+;; [51 :WP-peasants-against-rat]
+;; [18 :SP-peasants-against-demon]
+;;
+;; [56 :WP-subak-fails-against-rat]
+;; [62 :WP-subak-succeeds-against-rat])
+;; [23 :SP-subak-fails-against-demon]
+;; [29 :SP-subak-succeeds-against-demon]
+;;
+;; [61 :WP-subak-succeed-rat->order]
+;; [55 :WP-subak-fail-rat->disorder]
+;; [28 :SP-subak-succeed-demon->order]
+;; [22 :SP-subak-fail-demon->disorder]
+
 
 (defpred Causal-if)
 (defpred Ceases)
@@ -28,9 +60,16 @@
 (defpred Is-pest)
 (defpred Is-enemy-of-state)
 (defpred Is-demon)
+;(defpred Is-king)
+;(defpred Is-negara)
+;(defpred Is-peasant)
+;(defpred Is-rice)
+;(defpred Is-sacred)
+;(defpred Is-subak)
 (defpred Member-of)
 (defpred Nourishes)
 (defpred Persists)
+;(defpred Preventative-if)  ; not currently used
 (defpred Shares)
 (defpred Struggles-alone)
 (defpred Struggles-together)
@@ -39,6 +78,7 @@
 
 (defobj king)
 (defobj state)
+;(defobj demon)
 (defobj demon)
 (defobj demon)
 (defobj water)
@@ -49,24 +89,53 @@
 (defobj subak)
 (defobj rat)
 
-;; Note these have no effect if there is no map node that maps the two predicates.
+;; Note two things:
+;; These have no effect if there is no map node that maps the two predicates.
 ;; There can only be a map node if there exist propositions using one predicate
 ;; in one of the major analogues (i.e. "source" and "target", formerly), and one
 ;; in the other.  Here the major analogues are "worldly" and "spiritual".
 (def conceptual-relats 
-  [[-1.0 :Is-ordered :Is-disordered]
+  [;[-1.0 :Causal-if :Preventative-if]  ; at present there are no such propns
+   ;[-1.0 :Is-king :Is-peasant]
+   ;[-0.9 :Is-king :Is-subak]
+   ;[-1.0 :Is-pest :Is-enemy-of-state] ; no effect since these would be used only in worldly propositions
+   ;[-1.0 :Is-pest :Is-demon]
+   ;[-1.0 :Is-enemy-of-state :Is-demon]
+   ;[-1.0 :Struggles-together :Struggles-alone]      ; applies to no nodes
+   ;[-1.0 :Struggles-together :Struggles-on-brehalf] ; applies to no nodes
+   [-1.0 :Is-ordered :Is-disordered]
    [-1.0 :Persists :Ceases]
    [-1.0 :Succeeds :Fails]])
 
+; examples:
+; (def semantic-iffs [[-0.1 :CB-vpp :V-ipa] [-0.1 :CV-rpa :B-abp]])
+; (def semantic-iffs [[-0.1 :B-king :P-subak]])
 (def semantic-iffs [
-                    ;[-0.1 :WP-peasants-against-rat :SB-king-against-demon] ; (check) adding this links the 2-node subnet into the rest in siobhan e.g. but the negative activn isn't flowing into the other node:
+                    ; TODO EXPERIMENT:
+                    ; adding this links the 2-node subnet into the rest in siobhan e.g.
+                    ; but the negative activn isn't flowing into the other node.
+                    ;; TODO Question: In what sense do I ignore negative activns?
+                    ;; Is that what's supposed to happen *in the propn net*??
+                    ;[-0.1 :WP-peasants-against-rat :SB-king-against-demon]
                     ])
+
+;; QUESTIONS:
+;; There are several completely parallel propns here--i.e. identical but for the propn name/id.
+;; This is reasonable for some e.g. wrt demons, and I suppose you could say that e.g. king and state
+;; have both spiritual and worldyl aspects, so that Is-king has a slightly different spiritual and
+;; worldly meaning.  Maybe that's a stretch.  However, is it at all reasonable to talk of persisting
+;; as having both spiritual and worldly versions??
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; "WORLDLY" PROPOSITIONS (one of the two major analogues--one side of the mappings in the analogy net)
 
 (def worldly-brahmanic-propns 
-  [(defpropn WB-water-nourishes-state Nourishes [water state])  ;; "nourishes"??  "state"?? 
+  [;(defpropn WB-demon-bhutakala Is-bhutakala [demon])
+   ;(defpropn WB-king Is-king [king])
+   ;(defpropn WB-state Is-negara [state])
+   ;(defpropn WB-enemy-bhutakala Is-bhutakala [enemy])
+   ;(defpropn WB-is-enemy Is-enemy-of-state [enemy])
+   (defpropn WB-water-nourishes-state Nourishes [water state])  ;; "nourishes"??  "state"?? 
    (defpropn WB-king-for-subaks Struggles-on-behalf [king subak])
    (defpropn WB-king-for-peasant1 Struggles-on-behalf [king peasant1])
    (defpropn WB-king-for-peasant2 Struggles-on-behalf [king peasant2])
@@ -83,7 +152,12 @@
    (defpropn WB-state-disordered->ceases Causal-if [WB-state-disordered WB-state-ceases])])
 
 (def worldly-peasant-propns
-  [(defpropn WP-peasant1-in-subak Member-of [peasant1 subak])
+  [;(defpropn WP-demon-bhutakala Is-bhutakala [demon])
+   ;(defpropn WP-peasant1 Is-peasant [peasant1])
+   ;(defpropn WP-peasant2 Is-peasant [peasant2])
+   ;(defpropn WP-subak Is-subak [subak])
+   ;(defpropn WP-is-pest Is-pest [rat])
+   (defpropn WP-peasant1-in-subak Member-of [peasant1 subak])
    (defpropn WP-peasant2-in-subak Member-of [peasant2 subak])
    (defpropn WP-subak-ordered Is-ordered [subak])
    (defpropn WP-subak-disordered Is-disordered [subak])
@@ -92,9 +166,18 @@
    (defpropn WP-subak-ceases Ceases [subak])  ; should always receive negative activation
    (defpropn WP-subak-disordered->persists Causal-if [WP-subak-disordered WP-subak-persists]) ; NOTE this differs from Brahmanic
    (defpropn WP-subak-shares-water Shares [subak water]) ; Should really have multiple subaks as args; this is a simplification.
+   ;(defpropn WP-water-nourishes-peasant1 Nourishes [water peasant1])  ;; "nourishes"?? ;
+   ;(defpropn WP-water-nourishes-peasant2 Nourishes [water peasant2]) ;
+   ;(defpropn WP-water-peasant1-ordered Is-ordered [WP-water-nourishes-peasant1])
+   ;(defpropn WP-water-peasant2-ordered Is-ordered [WP-water-nourishes-peasant2])
+   ;(defpropn WP-rice Is-rice [rice])
+   ;(defpropn WP-rat-bhutakala Is-bhutakala [rat])
    (defpropn WP-water-nourishes-rice Nourishes [water rice]) ;
    (defpropn WP-water-rice-ordered Is-ordered [WP-water-nourishes-rice])
    (defpropn WP-peasants-against-rat Struggles-together [peasant1 peasant2 rat])
+   ;(defpropn WP-subak-against-rat Struggles-alone [subak rat])
+   ;(defpropn WP-peasants->subak-against-rat Causal-if [WP-peasants-against-rat WP-subak-against-rat])
+   ;(defpropn WP-subak-succeeds-against-rat Succeeds [WP-subak-against-rat])
    (defpropn WP-subak-succeeds-against-rat Succeeds [WP-peasants-against-rat])
    (defpropn WP-subak-succeed-rat->order Causal-if [WP-subak-succeeds-against-rat WP-subak-ordered])
    (defpropn WP-subak-fails-against-rat Fails [WP-peasants-against-rat])
@@ -104,7 +187,13 @@
 ;; "SPIRITUAL" PROPOSITIONS (one of the two major analogues--one side of the mappings in the analogy net)
 
 (def spiritual-brahmanic-propns 
-  [(defpropn SB-water-nourishes-state Nourishes [water state])  ;; "nourishes"??  "state"?? 
+  [;(defpropn SB-demon-bhutakala Is-bhutakala [demon])
+   ;(defpropn SB-king Is-king [king])
+   ;(defpropn SB-state Is-negara [state])
+   ;(defpropn SB-water-sacred Is-sacred [water])
+   (defpropn SB-water-nourishes-state Nourishes [water state])  ;; "nourishes"??  "state"?? 
+   ;(defpropn SB-water-state-ordered Is-ordered [SB-water-nourishes-state])
+   ;(defpropn SB-demon Is-demon [demon])
    (defpropn SB-king-for-subaks Struggles-on-behalf [king subak])
    (defpropn SB-king-for-peasant1 Struggles-on-behalf [king peasant1])
    (defpropn SB-king-for-peasant2 Struggles-on-behalf [king peasant2])
@@ -121,7 +210,13 @@
    (defpropn SB-state-disordered->ceases Causal-if [SB-state-disordered SB-state-ceases])])
 
 (def spiritual-peasant-propns
-  [(defpropn SP-peasant1-in-subak Member-of [peasant1 subak])
+  [;(defpropn SP-demon-bhutakala Is-bhutakala [demon])
+   ;(defpropn SP-peasant1 Is-peasant [peasant1])
+   ;(defpropn SP-peasant2 Is-peasant [peasant2])
+   ;(defpropn SP-subak Is-subak [subak])
+   ;(defpropn SP-water-sacred Is-sacred [water])
+   ;(defpropn SP-demon Is-demon [demon])
+   (defpropn SP-peasant1-in-subak Member-of [peasant1 subak])
    (defpropn SP-peasant2-in-subak Member-of [peasant2 subak])
    (defpropn SP-subak-ordered Is-ordered [subak])
    (defpropn SP-subak-disordered Is-disordered [subak])
@@ -135,6 +230,9 @@
    (defpropn SP-water-peasant1-ordered Is-ordered [SP-water-nourishes-peasant1])
    (defpropn SP-water-peasant2-ordered Is-ordered [SP-water-nourishes-peasant2])
    (defpropn SP-peasants-against-demon Struggles-together [peasant1 peasant2 demon])
+   ;(defpropn SP-subak-against-demon Struggles-alone [subak demon])
+   ;(defpropn SP-peasants->subak-against-demon Causal-if [SP-peasants-against-demon SP-subak-against-demon])
+   ;(defpropn SP-subak-succeeds-against-demon Succeeds [SP-subak-against-demon])
    (defpropn SP-subak-succeeds-against-demon Succeeds [SP-peasants-against-demon])
    (defpropn SP-subak-succeed-demon->order Causal-if [SP-subak-succeeds-against-demon SP-subak-ordered])
    (defpropn SP-subak-fails-against-demon Fails [SP-peasants-against-demon])
