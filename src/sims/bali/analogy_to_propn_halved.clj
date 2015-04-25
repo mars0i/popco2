0e06f558c6ac2c84284375db8958cbc0064548f5
mabrams <mabrams@uab.edu>
2015-04-25 13:57:11 -0500

    trivial modifications of comments

--- src/sims/bali/analogy_to_propn_halved.clj
+++ src/sims/bali/analogy_to_propn_halved.clj
@@ -6,7 +6,8 @@
 (ns sims.bali.analogy-to-propn-halved)
 
 ;; Override definition of trust in popco.core.constants.
-;; Why it's OK to do this during initialization, and what to watch out for: http://stackoverflow.com/questions/5181367/is-defn-thread-safe .
+;; Why it's OK to do this during initialization, and what to watch out for:
+;; http://stackoverflow.com/questions/5181367/is-defn-thread-safe .
 (in-ns 'popco.core.constants)
 (def analogy-to-propn-pos-multiplier (* 0.5 analogy-to-propn-pos-multiplier))
 (def analogy-to-propn-neg-multiplier (* 0.5 analogy-to-propn-neg-multiplier))
