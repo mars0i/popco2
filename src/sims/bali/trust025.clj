f0147243504bd1eef47e8af36933304d948ff737
mabrams <mabrams@uab.edu>
2015-04-25 13:29:50 -0500

    trivial reformatting of comments

--- src/sims/bali/trust025.clj
+++ src/sims/bali/trust025.clj
@@ -6,7 +6,8 @@
 (ns sims.bali.trust025)
 
 ;; Override definition of trust in popco.core.constants.
-;; Why it's OK to do this during initialization, and what to watch out for: http://stackoverflow.com/questions/5181367/is-defn-thread-safe .
+;; Why it's OK to do this during initialization, and what to watch out for:
+;; http://stackoverflow.com/questions/5181367/is-defn-thread-safe .
 (in-ns 'popco.core.constants)
 (def trust 0.025)
 (in-ns 'sims.bali.trust025)
