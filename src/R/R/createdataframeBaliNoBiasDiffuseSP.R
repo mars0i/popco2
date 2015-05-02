10695bb9a418a2d2265b1f782dd171e93e5e7e96
mabrams <mabrams@uab.edu>
2015-05-02 14:26:24 -0500

    new scripts for new model

--- src/R/R/createdataframeBaliNoBiasDiffuseSP.R
+++ src/R/R/createdataframeBaliNoBiasDiffuseSP.R
@@ -1,10 +1,8 @@
-UNFINISHED
-
 source("~/p2/src/R/R/df2ra.R")
 gc()
 load("bali.rdata")
 gc()
-bali.df <- multiRAs2combinedMeanDF(list(bali[3:120,,,]), c("peasants"), "SB", "SP")
+bali.df <- multiRAs2combinedMeanDF(list(bali[3:100,,,]), c("peasants"), "SB", "SP")
 gc()
 save(bali.df, file="balidf.rdata")
 
---
+++ src/qsub/createdataframeBaliWhenReadyNobiasDiffuseSP.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+if [ -z "$1" ]; then
+	echo usage: `basename $0` jobid
+	echo "checks every 10 minutes to see if qstat still shows job jobid, and"
+	echo "runs createdataframeBali.R when it's done."
+	exit 1
+fi
+
+# wait until first job is done, checking every 10 minutes (hope for no error)
+while [ -n "`qstat | grep $1`" ] ; do sleep 10m ; done
+
+# job is done
+
+if [ -f bali.rdata ]; then
+	cd ~/data
+	qsub -l h_rt=18:00:00,vf=30G -N finddisg ~/p2/src/qsub/submitanything.job Rscript --no-init-file --verbose ~/p2/src/R/R/finddisagreementBaliNobiasDiffuseSP.R
+	qsub -l h_rt=18:00:00,vf=40G -N createdf ~/p2/src/qsub/submitanything.job Rscript --no-init-file --verbose ~/p2/src/R/R/createdataframeBaliNobiasDiffuseSP.R
+else
+	echo `basename $0`: datafile missing
+	exit 2
+fi
