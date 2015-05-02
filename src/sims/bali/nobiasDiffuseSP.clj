059ac2a0650853a991d642f5689f31091bb7cb7c
mabrams <mabrams@uab.edu>
2015-05-02 14:20:25 -0500

    moved notes to md file, added comment about new bug

--- src/sims/bali/nobiasDiffuseSP.clj
+++ src/sims/bali/nobiasDiffuseSP.clj
@@ -5,48 +5,16 @@
 ;; Goal is to have peasant spiritual propns diffuse, and gradually enter into
 ;; the analogy net, and thus gradually come to be selected in contrast to
 ;; spiritual brahmanic propns, in response to worldly-peasant propns.
+;; SEE nobiasDiffuseSP.md for more notes
 
-;; SHOULDN'T IT BE WORLDLY PEASANT PROPNS THAT ARE PERCEIVED, NOT SPIRITUAL PROPNS?
-
-;; innov is the person who's supposed to diffuse the spiritual peasant propns.
-;; she's the innovator.  Wait--is that necessary?  the True Believer and the
-;; Naysayer are already doing that.  Um, unless I restrict them to only asserting
-;; non-SP propns.
-
-;; Question: What do I want the pundits to do???
-
-;; Q: How did I do this in the Sanday sims in "Moderate Role"?
-;; A: I gave most members of pop all propositions except the ones to
-;; be diffused (the earth origin propns).  Then for each earth origin
-;; propn, added one indiv that had all of the propns that the other
-;; indvivs had, plus a unique earth origin propn.  These played the
-;; pundit role.  The diffusion happened while the contrary worldly
-;; analog, hunting, was salient.  Then I switched the salience from
-;; hunting to parenting, and the earth origin propns kicked in.
-
-;; The thing is, I want the propns to diffuse, but I don't want them to
-;; necessarily have high activation.  I just want them available in the
-;; analogy net.  They have to have a certain amount of activation in 
-;; order to get uttered.  But I want perception of the environment
-;; to be the driver of proposition activation.  I don't have these
-;; things separated.  Then again, I don't think the diffused propns
-;; were perceived in the Sanday model 2 simulation.
-;; Yep. "... they do end up getting activations other than zero, due to 
-;; mappings in which they participate imperfectly."
-
-;; OK so maybe this is what I want:
-;; innov, The innovator is just another peasant, except that she has the new
-;; spiritual peasant ideas as well as the other three domains.
-;; 
-;; This will work for the no-bias case, I think, but maybe not for the
-;; success bias case, since then she will have a complete set of spiritual
-;; propns.  Won't that make her more successful, i.e. give her higher values
-;; of worldly peasant propns?  Well not if I make success contingent on 
-;; the local group, but that's a later stage of modeling.
-;;
-;; So maybe I need to do it the Sanday model 2 way, and split the spiritual
-;; propns up between diff indivs.
-
+;; THIS DOESN'T WORK.  ALL ACTIVATIONS REMAINED ZERO FOR ALL TICKS IN ALL RUNS.
+;; EVEN THE PUNDITS.
+;; WTF?
+;; TODO HERE's THE BUG (?):
+;; I think the problem is that the pundits have propn nets that peg only
+;; spiritual propns, but they unmask only worldly propns.  therefore none
+;; of the pegged propns are unmasked.
+;; Although that's not what I see, but maybe the mask has already been applied before I see the activns.
 
 (ns sims.bali.nobiasDiffuseSP
   (:require [popco.core.person :as pers]
@@ -55,13 +23,20 @@
             [popco.nn.analogy :as an]
             [sims.bali.collections :as c]))
 
-(let [sp0 (nth c/spiritual-peasant-propns 0), sp1 (nth c/spiritual-peasant-propns 1), sp2 (nth c/spiritual-peasant-propns 2), sp3 (nth c/spiritual-peasant-propns 3),
+;; The many indiv bindings below allow adding individual SP propns to persons.
+;; Maybe in future use a macro or something to define all of this stuff automatically.
+
+(let [;; an abbreviation for every propn in the SP group:
+      sp0 (nth c/spiritual-peasant-propns 0), sp1 (nth c/spiritual-peasant-propns 1), sp2 (nth c/spiritual-peasant-propns 2), sp3 (nth c/spiritual-peasant-propns 3),
       sp4 (nth c/spiritual-peasant-propns 4), sp5 (nth c/spiritual-peasant-propns 5), sp6 (nth c/spiritual-peasant-propns 6), sp7 (nth c/spiritual-peasant-propns 7),
       sp8 (nth c/spiritual-peasant-propns 8), sp9 (nth c/spiritual-peasant-propns 9), sp10 (nth c/spiritual-peasant-propns 10), sp11 (nth c/spiritual-peasant-propns 11),
       sp12 (nth c/spiritual-peasant-propns 12), sp13 (nth c/spiritual-peasant-propns 13), sp14 (nth c/spiritual-peasant-propns 14), sp15 (nth c/spiritual-peasant-propns 15),
       sp16 (nth c/spiritual-peasant-propns 16), sp17 (nth c/spiritual-peasant-propns 17)
 
+      ;; collection of all propns except the SP propns:
       all-but-SP-propns (concat c/spiritual-brahmanic-propns c/worldly-peasant-propns)
+
+      ;; collections of propns with all of the non-SP propns, plus one SP propn:
       just-one-SP-propns-00 (conj all-but-SP-propns sp0)
       just-one-SP-propns-01 (conj all-but-SP-propns sp1)
       just-one-SP-propns-02 (conj all-but-SP-propns sp2)
@@ -81,7 +56,10 @@
       just-one-SP-propns-16 (conj all-but-SP-propns sp16)
       just-one-SP-propns-17 (conj all-but-SP-propns sp17)
 
+      ;; an analogy net made from all propns except the SP propns:
       all-but-SP-anet (an/make-analogy-net c/spiritual-brahmanic-propns c/worldly-propns c/conceptual-relats) ;; DO I WANT WORLDLY BRAHMANIC PROPNS??
+
+      ;; analogy nets made from all of the non-SP propns, plus one SP propn:
       just-one-SP-anet-00 (an/make-analogy-net (conj c/spiritual-brahmanic-propns sp0) c/worldly-propns c/conceptual-relats)
       just-one-SP-anet-01 (an/make-analogy-net (conj c/spiritual-brahmanic-propns sp1) c/worldly-propns c/conceptual-relats)
       just-one-SP-anet-02 (an/make-analogy-net (conj c/spiritual-brahmanic-propns sp2) c/worldly-propns c/conceptual-relats)
---
+++ src/sims/bali/nobiasDiffuseSP.md
@@ -0,0 +1,49 @@
+notes on nobiasDiffiuseSP.clj
+====
+
+TODO IN PROGRESS
+
+Goal is to have peasant spiritual propns diffuse, and gradually enter into
+the analogy net, and thus gradually come to be selected in contrast to
+spiritual brahmanic propns, in response to worldly-peasant propns.
+
+SHOULDN'T IT BE WORLDLY PEASANT PROPNS THAT ARE PERCEIVED, NOT SPIRITUAL PROPNS?
+
+innov is the person who's supposed to diffuse the spiritual peasant propns.
+she's the innovator.  Wait--is that necessary?  the True Believer and the
+Naysayer are already doing that.  Um, unless I restrict them to only asserting
+non-SP propns.
+
+Question: What do I want the pundits to do???
+
+Q: How did I do this in the Sanday sims in "Moderate Role"?
+A: I gave most members of pop all propositions except the ones to
+be diffused (the earth origin propns).  Then for each earth origin
+propn, added one indiv that had all of the propns that the other
+indvivs had, plus a unique earth origin propn.  These played the
+pundit role.  The diffusion happened while the contrary worldly
+analog, hunting, was salient.  Then I switched the salience from
+hunting to parenting, and the earth origin propns kicked in.
+
+The thing is, I want the propns to diffuse, but I don't want them to
+necessarily have high activation.  I just want them available in the
+analogy net.  They have to have a certain amount of activation in 
+order to get uttered.  But I want perception of the environment
+to be the driver of proposition activation.  I don't have these
+things separated.  Then again, I don't think the diffused propns
+were perceived in the Sanday model 2 simulation.
+Yep. "... they do end up getting activations other than zero, due to 
+mappings in which they participate imperfectly."
+
+OK so maybe this is what I want:
+innov, The innovator is just another peasant, except that she has the new
+spiritual peasant ideas as well as the other three domains.
+
+This will work for the no-bias case, I think, but maybe not for the
+success bias case, since then she will have a complete set of spiritual
+propns.  Won't that make her more successful, i.e. give her higher values
+of worldly peasant propns?  Well not if I make success contingent on 
+the local group, but that's a later stage of modeling.
+
+So maybe I need to do it the Sanday model 2 way, and split the spiritual
+propns up between diff indivs.
