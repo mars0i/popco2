af6879e2c710ce2c3ec83350f334c81c062242e4
mars0i <mabrams@uab.edu>
2015-05-01 23:13:31 -0500

    formatting, comments

--- src/sims/bali/nobiasDiffuseSP.clj
+++ src/sims/bali/nobiasDiffuseSP.clj
@@ -6,6 +6,8 @@
 ;; the analogy net, and thus gradually come to be selected in contrast to
 ;; spiritual brahmanic propns, in response to worldly-peasant propns.
 
+;; SHOULDN'T IT BE WORLDLY PEASANT PROPNS THAT ARE PERCEIVED, NOT SPIRITUAL PROPNS?
+
 ;; innov is the person who's supposed to diffuse the spiritual peasant propns.
 ;; she's the innovator.  Wait--is that necessary?  the True Believer and the
 ;; Naysayer are already doing that.  Um, unless I restrict them to only asserting
@@ -102,25 +104,25 @@
 ;; args:                        ID       UNMASKED                 PROPN-NET                 ANALOGY-NET         UTTERABLE-IDS                 GROUPS      TALK-TO-GROUPS MAX-TALK-TO BIAS-FILTER QUALITY-FN
       aat     (pers/make-person :aat     c/worldly-peasant-propns c/spiritual-perc-pnet     c/anet              c/spiritual-propn-ids         [:pundits]  [:peasants]    1           nil         (constantly 1))
       aaf     (pers/make-person :aaf     c/worldly-peasant-propns c/spiritual-neg-perc-pnet c/anet              c/spiritual-propn-ids         [:pundits]  [:peasants]    1           nil         (constantly 1))
-      innov00 (pers/make-person :innov00 just-one-SP-propns-00     c/no-perc-pnet           just-one-SP-anet-00 c/spiritual-peasant-propn-ids [:peasants] [:peasants]    1           nil         (constantly 1))
-      innov01 (pers/make-person :innov01 just-one-SP-propns-01     c/no-perc-pnet           just-one-SP-anet-01 c/spiritual-peasant-propn-ids [:peasants] [:peasants]    1           nil         (constantly 1))
-      innov02 (pers/make-person :innov02 just-one-SP-propns-02     c/no-perc-pnet           just-one-SP-anet-02 c/spiritual-peasant-propn-ids [:peasants] [:peasants]    1           nil         (constantly 1))
-      innov03 (pers/make-person :innov03 just-one-SP-propns-03     c/no-perc-pnet           just-one-SP-anet-03 c/spiritual-peasant-propn-ids [:peasants] [:peasants]    1           nil         (constantly 1))
-      innov04 (pers/make-person :innov04 just-one-SP-propns-04     c/no-perc-pnet           just-one-SP-anet-04 c/spiritual-peasant-propn-ids [:peasants] [:peasants]    1           nil         (constantly 1))
-      innov05 (pers/make-person :innov05 just-one-SP-propns-05     c/no-perc-pnet           just-one-SP-anet-05 c/spiritual-peasant-propn-ids [:peasants] [:peasants]    1           nil         (constantly 1))
-      innov06 (pers/make-person :innov06 just-one-SP-propns-06     c/no-perc-pnet           just-one-SP-anet-06 c/spiritual-peasant-propn-ids [:peasants] [:peasants]    1           nil         (constantly 1))
-      innov07 (pers/make-person :innov07 just-one-SP-propns-07     c/no-perc-pnet           just-one-SP-anet-07 c/spiritual-peasant-propn-ids [:peasants] [:peasants]    1           nil         (constantly 1))
-      innov08 (pers/make-person :innov08 just-one-SP-propns-08     c/no-perc-pnet           just-one-SP-anet-08 c/spiritual-peasant-propn-ids [:peasants] [:peasants]    1           nil         (constantly 1))
-      innov09 (pers/make-person :innov09 just-one-SP-propns-09     c/no-perc-pnet           just-one-SP-anet-09 c/spiritual-peasant-propn-ids [:peasants] [:peasants]    1           nil         (constantly 1))
-      innov10 (pers/make-person :innov10 just-one-SP-propns-10     c/no-perc-pnet           just-one-SP-anet-10 c/spiritual-peasant-propn-ids [:peasants] [:peasants]    1           nil         (constantly 1))
-      innov11 (pers/make-person :innov11 just-one-SP-propns-11     c/no-perc-pnet           just-one-SP-anet-11 c/spiritual-peasant-propn-ids [:peasants] [:peasants]    1           nil         (constantly 1))
-      innov12 (pers/make-person :innov12 just-one-SP-propns-12     c/no-perc-pnet           just-one-SP-anet-12 c/spiritual-peasant-propn-ids [:peasants] [:peasants]    1           nil         (constantly 1))
-      innov13 (pers/make-person :innov13 just-one-SP-propns-13     c/no-perc-pnet           just-one-SP-anet-13 c/spiritual-peasant-propn-ids [:peasants] [:peasants]    1           nil         (constantly 1))
-      innov14 (pers/make-person :innov14 just-one-SP-propns-14     c/no-perc-pnet           just-one-SP-anet-14 c/spiritual-peasant-propn-ids [:peasants] [:peasants]    1           nil         (constantly 1))
-      innov15 (pers/make-person :innov15 just-one-SP-propns-15     c/no-perc-pnet           just-one-SP-anet-15 c/spiritual-peasant-propn-ids [:peasants] [:peasants]    1           nil         (constantly 1))
-      innov16 (pers/make-person :innov16 just-one-SP-propns-16     c/no-perc-pnet           just-one-SP-anet-16 c/spiritual-peasant-propn-ids [:peasants] [:peasants]    1           nil         (constantly 1))
-      innov17 (pers/make-person :innov17 just-one-SP-propns-17     c/no-perc-pnet           just-one-SP-anet-17 c/spiritual-peasant-propn-ids [:peasants] [:peasants]    1           nil         (constantly 1))
-      peasant (pers/make-person :peas    all-but-SP-propns         c/no-perc-pnet           all-but-SP-anet     c/spiritual-propn-ids         [:peasants] [:peasants]    1           nil         (constantly 1))
+      innov00 (pers/make-person :innov00 just-one-SP-propns-00    c/no-perc-pnet            just-one-SP-anet-00 c/spiritual-peasant-propn-ids [:peasants] [:peasants]    1           nil         (constantly 1))
+      innov01 (pers/make-person :innov01 just-one-SP-propns-01    c/no-perc-pnet            just-one-SP-anet-01 c/spiritual-peasant-propn-ids [:peasants] [:peasants]    1           nil         (constantly 1))
+      innov02 (pers/make-person :innov02 just-one-SP-propns-02    c/no-perc-pnet            just-one-SP-anet-02 c/spiritual-peasant-propn-ids [:peasants] [:peasants]    1           nil         (constantly 1))
+      innov03 (pers/make-person :innov03 just-one-SP-propns-03    c/no-perc-pnet            just-one-SP-anet-03 c/spiritual-peasant-propn-ids [:peasants] [:peasants]    1           nil         (constantly 1))
+      innov04 (pers/make-person :innov04 just-one-SP-propns-04    c/no-perc-pnet            just-one-SP-anet-04 c/spiritual-peasant-propn-ids [:peasants] [:peasants]    1           nil         (constantly 1))
+      innov05 (pers/make-person :innov05 just-one-SP-propns-05    c/no-perc-pnet            just-one-SP-anet-05 c/spiritual-peasant-propn-ids [:peasants] [:peasants]    1           nil         (constantly 1))
+      innov06 (pers/make-person :innov06 just-one-SP-propns-06    c/no-perc-pnet            just-one-SP-anet-06 c/spiritual-peasant-propn-ids [:peasants] [:peasants]    1           nil         (constantly 1))
+      innov07 (pers/make-person :innov07 just-one-SP-propns-07    c/no-perc-pnet            just-one-SP-anet-07 c/spiritual-peasant-propn-ids [:peasants] [:peasants]    1           nil         (constantly 1))
+      innov08 (pers/make-person :innov08 just-one-SP-propns-08    c/no-perc-pnet            just-one-SP-anet-08 c/spiritual-peasant-propn-ids [:peasants] [:peasants]    1           nil         (constantly 1))
+      innov09 (pers/make-person :innov09 just-one-SP-propns-09    c/no-perc-pnet            just-one-SP-anet-09 c/spiritual-peasant-propn-ids [:peasants] [:peasants]    1           nil         (constantly 1))
+      innov10 (pers/make-person :innov10 just-one-SP-propns-10    c/no-perc-pnet            just-one-SP-anet-10 c/spiritual-peasant-propn-ids [:peasants] [:peasants]    1           nil         (constantly 1))
+      innov11 (pers/make-person :innov11 just-one-SP-propns-11    c/no-perc-pnet            just-one-SP-anet-11 c/spiritual-peasant-propn-ids [:peasants] [:peasants]    1           nil         (constantly 1))
+      innov12 (pers/make-person :innov12 just-one-SP-propns-12    c/no-perc-pnet            just-one-SP-anet-12 c/spiritual-peasant-propn-ids [:peasants] [:peasants]    1           nil         (constantly 1))
+      innov13 (pers/make-person :innov13 just-one-SP-propns-13    c/no-perc-pnet            just-one-SP-anet-13 c/spiritual-peasant-propn-ids [:peasants] [:peasants]    1           nil         (constantly 1))
+      innov14 (pers/make-person :innov14 just-one-SP-propns-14    c/no-perc-pnet            just-one-SP-anet-14 c/spiritual-peasant-propn-ids [:peasants] [:peasants]    1           nil         (constantly 1))
+      innov15 (pers/make-person :innov15 just-one-SP-propns-15    c/no-perc-pnet            just-one-SP-anet-15 c/spiritual-peasant-propn-ids [:peasants] [:peasants]    1           nil         (constantly 1))
+      innov16 (pers/make-person :innov16 just-one-SP-propns-16    c/no-perc-pnet            just-one-SP-anet-16 c/spiritual-peasant-propn-ids [:peasants] [:peasants]    1           nil         (constantly 1))
+      innov17 (pers/make-person :innov17 just-one-SP-propns-17    c/no-perc-pnet            just-one-SP-anet-17 c/spiritual-peasant-propn-ids [:peasants] [:peasants]    1           nil         (constantly 1))
+      peasant (pers/make-person :peas    all-but-SP-propns        c/no-perc-pnet            all-but-SP-anet     c/spiritual-propn-ids         [:peasants] [:peasants]    1           nil         (constantly 1))
      ] 
 
       
