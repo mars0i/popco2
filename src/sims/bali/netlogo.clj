ebc53f2e9990e1ed118f73a392496b72f99b84cd
mars0i <mabrams@uab.edu>
2015-12-05 17:50:26 -0600

    Added initial version of guts of bali-init

--- src/sims/bali/netlogo.clj
+++ src/sims/bali/netlogo.clj
@@ -3,16 +3,26 @@
 ;;; specified in the file LICENSE.
 
 (ns sims.bali.netlogo
-  (:require [popco.core.main :as mn]))
+  (:require [popco.core.main :as mn]
+            [popco.core.person :as prs]
+            [popco.core.population :as pp]
+            ;[popco.communic.listen :as cl]
+            [popco.nn.analogy :as an]
+            [sims.bali.collections :as c]))
 
-(def *popn* (atom nil))
+(def num-subaks 172)
 
+(def *popn* (atom "not-yet-defined"))
 
-(defn bali-init
-  "Initialize population of 172 popco persons, which represent subaks."
-  []
-  42)
+(defn bali-init "Initialize population of popco persons representing subaks." []
+               ;;               ID   UNMASKED   PROPN-NET      ANALOGY-NET UTTERABLE-IDS         GROUPS      TALK-TO-GROUPS MAX-TALK-TO BIAS-FILTER QUALITY-FN
+  (let [subak (prs/make-person :temp all-propns c/no-perc-pnet c/anet     c/spiritual-propn-ids ["ignored"] ["ignored"]    num-subaks  nil         prs/constantly1)]
+    (reset! *popn*
+            (map (partial new-person-from-old subak)
+                 (map double (range num-subaks)))))) ; subak ids: Doubles from 0 to num-subaks. that's what NetLogo will send.
 
 (defn bali-once
   [subak-ids]
-  (reverse subak-ids)) ; testing
+  ;; (swap! *popn* <set talk-to-persons from input, run one tick>)
+  ;; return per-subak average worldly activn vals
+  (reverse subak-ids)) ; for testing only
